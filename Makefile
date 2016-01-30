275a97a0c6de461cc43df20b361de041b3ac2647
Ean Moody <moody.designer@gmail.com>
2016-01-30 16:24:53 -0500

    Revert "Move _themes and conf.py up to root"
    This reverts commit edc57c5c58084b1cfed97e498488e6fa12d48c6f.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 #
 
 # You can set these variables from the command line.
-SPHINXOPTS    = -c .
+SPHINXOPTS    =
 SPHINXBUILD   = sphinx-build
 PAPER         =
 BUILDDIR      = build
--- source/conf.py
+++ source/conf.py
@@ -34,7 +34,7 @@ extensions = [
 
 # Add any paths that contain templates here, relative to this directory.
 html_theme = "open5e_red_theme"
-html_theme_path = ["_themes"]
+html_theme_path = ["../_themes"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
