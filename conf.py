e2d26e5a2db9d9c1c7f3543bd3ca7073292093b5
Max Rothman <max@edx.org>
2016-04-02 11:07:59 -0400

    Remove html suffix from all pages

--- conf.py
+++ conf.py
@@ -196,7 +196,7 @@ html_show_copyright = True
 #html_use_opensearch = ''
 
 # This is the file name suffix for HTML files (e.g. ".xhtml").
-#html_file_suffix = None
+html_file_suffix = ''
 
 # Language to be used for generating the HTML full-text search index.
 # Sphinx supports the following languages:
