e19ba23274cbe63e77514057fc6701498070a6c0
eepMoody <moody.designer@gmail.com>
2018-08-21 22:45:57 -0400

    add models for new data types

--- server/api/models.py
+++ server/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
 
@@ -50,6 +51,67 @@ class Spell(models.Model):
     dnd_class = models.TextField()
     archetype = models.TextField()
     circles = models.TextField()
+
+class Archetype(models.Model):
+    name = models.TextField()
+    desc = models.TextField()
+
+class CharClass(models.Model):
+    hit_dice = models.TextField()
+    hp_at_1st_level = models.TextField()
+    hp_at_higher_levels = models.TextField()
+    prof_armor = models.TextField()
+    prof_weapons = models.TextField()
+    prof_tools = models.TextField()
+    prof_saving_throws = models.TextField()
+    prof_skills = models.TextField()
+    equipment = models.TextField()
+    table = models.TextField()
+    desc = models.TextField()
+    spellcasting_ability = models.TextField()
+    subtypes_name = models.TextField()
+    archetype = models.ForeignKey(Archetype, on_delete=models.CASCADE)
+
+class SubRace(models.Model):
+    name = models.TextField()
+    desc = models.TextField()
+    asi_desc = models.TextField()
+    asi = JSONField()
+    traits = models.TextField()
+
+class Race(models.Model):
+    name = models.TextField()
+    desc = models.TextField()
+    asi_desc = models.TextField()
+    asi = JSONField()
+    age = models.TextField()
+    alignment = models.TextField()
+    size = models.TextField()
+    speed_walk = models.IntegerField()
+    speed_desc = models.TextField()
+    languages = models.TextField()
+    vision = models.TextField()
+    traits = models.TextField()
+    subtypes  = models.TextField()
+    sub_race = models.ForeignKey(SubRace, on_delete=models.CASCADE)
+
+class Planes(models.Model):
+    name = models.TextField()
+    desc = models.TextField()
+
+class Sections(models.Model):
+    name = models.TextField()
+    desc = models.TextField()
+    parent = models.TextField()
+
+class Feats(models.Model):
+    name = models.TextField()
+    prerequisite = models.TextField()
+    desc = models.TextField()
+
+class Conditions(models.Model):
+    name = models.TextField
+    desc = models.TextField
 
 class GameContentDocument(models.Model):
     name = models.TextField() # System Reference Document
