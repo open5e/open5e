aecb58103d20a957ff057776866ef4bf327bf614
acidDrain <jmgrow@gmail.com>
2018-10-12 11:27:07 -0500

    Changed the variable HOST in Dockerfile and build script back to SERVER_NAME

--- docker/scripts/build_docker.sh
+++ docker/scripts/build_docker.sh
@@ -7,10 +7,10 @@ echo "....."
 
 [ -z "$DJANGO_SECRET" ] && { echo "Missing required environment variable DJANGO_SECRET"; exit 1; }
 [ -z "$OPEN5E_DEBUG" ] && { echo "Missing required environment variable OPEN5E_DEBUG"; exit 1; }
-[ -z "$HOST" ] && { echo "Missing required environment variable HOST"; exit 1; }
+[ -z "$SERVER_NAME" ] && { echo "Missing required environment variable SERVER_NAME"; exit 1; }
 
 docker build -f "$(dirname $BASE_DIR)/Dockerfile" \
   --build-arg DJANGO_SECRET="$DJANGO_SECRET" \
   --build-arg OPEN5E_DEBUG="$OPEN5E_DEBUG" \
-  --build-arg HOST="$HOST" \
+  --build-arg SERVER_NAME="$SERVER_NAME" \
   -t open5e-api .
