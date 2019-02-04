52ceb46575c2aef366e05d8026af42f250676de8
swarner <swarner@forbes.com>
2019-02-04 16:00:29 -0500

    make ascending and descending sort work

--- pages/spells/spell-tables.vue
+++ pages/spells/spell-tables.vue
@@ -10,25 +10,28 @@
         <table>
     <thead>
       <tr>
-        <th v-on:click="sort('name')">Name</th>
-        <th v-on:click="sort('school')">School</th>
-        <th v-on:click="sort('level')">Level</th>
-        <th v-on:click="sort('components')">Components</th>
-        <th v-on:click="sort('concentration')">Concentration</th>
+        <th class="spell-table-header" v-on:click="sort('name')">Name</th>
+        <th class="spell-table-header" v-on:click="sort('school')">School</th>
+        <th class="spell-table-header" v-on:click="sort('level')">Level</th>
+        <th class="spell-table-header" v-on:click="sort('components')">Component</th>
+        <th class="spell-table-header-class">Class</th>
       </tr>
     </thead>
     <tbody>
       <tr v-for="spell in spellsListed" :key="spell.name">
-        <td>{{spell.name}}</td>
+        <td>   <nuxt-link tag="a" 
+            :params="{id: spell.slug}" 
+            :to="`/spells/${spell.slug}`">{{spell.name}}</nuxt-link>
+        </td>
         <td>{{spell.school}}</td>
         <td>{{spell.level}}</td>
         <td>{{spell.components}}</td>
-        <td>{{spell.concentration}}</td>
+        <td><span v-for="(spellclass, index) in spell.dnd_class" :key="spellclass"><span class="dnd_class" v-on:click="filterByClass(spellclass)">{{spellclass}}</span><span v-if="index+1 < spell.dnd_class.length">, </span></span></td>
       </tr>
     </tbody>
   </table>
     </div>
-   <span style="display:none;"> sort={{currentSortProperty}}, dir={{currentSortDir}}</span>
+   <span style="display:none;">Sorting by sort={{currentSortProperty}}, dir={{currentSortDir}}</span>
   </section>
 </template>
 
@@ -44,6 +47,14 @@ export default {
     return axios.get(`${process.env.apiUrl}/spells/?fields=slug,name,dnd_class,school,level,concentration,components&limit=1000`) //you will need to enable CORS to make this work
     .then(response => {
       this.spells = response.data.results
+      // Until api sends arrays this will work to sort spells by class.
+      this.spells.map((item)=>{
+          item.dnd_class = item.dnd_class.split(",");
+          for(var i = 0; i < item.dnd_class.length; i++){
+              item.dnd_class[i].trim();
+          }
+      })
+    //
     })
   },
   data () {
@@ -51,7 +62,8 @@ export default {
       spells: [],
       filter: '', 
       currentSortProperty:'name',
-      currentSortDir:'asc'
+      currentSortDir:'asc',
+      sortByClass: null
     }
   },
   methods: {
@@ -63,9 +75,15 @@ export default {
     },
     sort:function(prop) {
         if(prop === this.currentSortProperty) {
-        this.currentSortPropertyDir = this.currentSortDir==='asc'?'desc':'asc';
+           this.currentSortDir = this.currentSortDir === 'asc'?'desc':'asc';
         }
         this.currentSortProperty = prop;
+        this.sortByClass = null;
+        this.spellsListed = {};
+    },
+    filterByClass : function(some_class){
+        this.sortByClass = some_class.trim();
+        console.log('filtering by... ' + this.sortByClass)
         this.spellsListed = {};
     }
   },
@@ -95,6 +113,14 @@ export default {
 </script>
 
 <style scoped>
+    .spell-table-header{
+        text-decoration: underline;
+        cursor: pointer;
+        vertical-align: baseline;
+    }
 
+    .spell-table-header-class{
+        vertical-align: baseline;
+    }
 </style>
 
