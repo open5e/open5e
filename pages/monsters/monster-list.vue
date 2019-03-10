eaf30b3b8cd98c0d33325b49782a26b9228fa052
Scott <scottbwarner@gmail.com>
2019-03-10 19:22:10 -0400

    Replace Monsters a-z List with sortable monters table (#293)

--- pages/monsters/monster-list.vue
+++ pages/monsters/monster-list.vue
@@ -4,34 +4,41 @@
       <span>Monster List</span>
       <filter-input v-on:input="updateFilter" placeholder="Filter monsters..."></filter-input>
     </h2>     
-    <div :class="{'three-column': !filter}">
-    <p v-if="!monsterListLength" >No results</p> 
-      <ul class="list--items" 
-        v-bind:key="letter[0].name.charAt(0)" 
-        v-for="(letter, key) in monstersByLetter">
-
-        <h3 v-if="!filter">{{key.toUpperCase()}}</h3>
-          <li v-bind:key="monster.name" v-for="monster in letter">
-            <nuxt-link tag="a" 
-              :params="{id: monster.slug}" 
-              :to="`/monsters/${monster.slug}`">
-
-              {{monster.name}}
-            </nuxt-link>
-          </li>
-      </ul>
+    <div>
+        <table>
+    <thead>
+      <tr>
+        <th class="monster-table-header" v-on:click="sort('name')">Name</th>
+        <th class="monster-table-header" v-on:click="sort('type')">Type</th>
+        <th class="monster-table-header" v-on:click="sort('challenge_rating')">CR</th>
+        <th class="monster-table-header" v-on:click="sort('size')">Size</th>
+          <th class="monster-table-header" v-on:click="sort('hit_points')">Hit Points</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="monster in monstersListed" :key="monster.name">
+        <td>   <nuxt-link tag="a" 
+            :params="{id: monster.slug}" 
+            :to="`/monsters/${monster.slug}`">{{monster.name}}</nuxt-link>
+        </td>
+        <td>{{monster.type}}</td>
+        <td>{{monster.challenge_rating}}</td>
+        <td>{{monster.size}}</td>
+        <td>{{monster.hit_points}}</td>
+      </tr>
+    </tbody>
+  </table>
     </div>
+      <span style="display:none;">Sorting by sort={{currentSortProperty}}, dir={{currentSortDir}}</span>
   </section>
 </template>
 
 <script>
 import axios from 'axios'
-import StatBonus from '~/components/StatBonus.vue'
 import FilterInput from '~/components/FilterInput.vue'
 
 export default {
   components: {
-    StatBonus,
     FilterInput
   },
   mounted () {
@@ -43,56 +50,60 @@ export default {
   data () {
     return {
       monsters: [],
-      filter: '',
+      filter: '', 
+      currentSortProperty:'challenge_rating',
+      currentSortDir:'asc'
     }
   },
   methods: {
-    slugify: function(text) {
-        return text.toString().toLowerCase()
-        .replace(/\s+/g, '-')           // Replace spaces with -
-        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-        .replace(/^-+/, '')             // Trim - from start of text
-        .replace(/-+$/, '');            // Trim - from end of text
-    },
     updateFilter: function(val) {
       this.filter = val;
-    }
-  },  
-  computed: {
-    monstersByLetter: function () {
-      let letters = {};
-      for (let i = 0; i < this.filteredMonsters.length; i++){ 
-        let firstLetter = this.filteredMonsters[i].name.charAt(0).toLowerCase(); 
-        if (!(firstLetter in letters)) {
-          letters[firstLetter] = [];
+    },
+    monsterListLength: function() { 
+      return Object.keys(this.monstersListed).length; 
+    },
+    sort:function(prop) {
+        if(prop === this.currentSortProperty) {
+           this.currentSortDir = this.currentSortDir === 'asc'?'desc':'asc';
         }
-        letters[firstLetter].push(this.filteredMonsters[i]); 
-      }
-      return letters
+        this.currentSortProperty = prop;
+        this.monstersListed = {};
+    }
+  }, 
+  computed: {
+     monstersListed:{
+       get: function(){
+          this.filteredMonsters.forEach(monster => monster.challenge_rating = eval(monster.challenge_rating));
+          return this.filteredMonsters
+        },
+        set: function () {
+            return this.filteredMonsters.sort((a,b) => {
+            let modifier = 1;
+            if(this.currentSortDir === 'desc') modifier = -1;
+                if(a[this.currentSortProperty] < b[this.currentSortProperty]) return -1 * modifier;
+                if(a[this.currentSortProperty] > b[this.currentSortProperty]) return 1 * modifier;
+                return 0;
+            });
+        }
     },
     filteredMonsters: function() { 
       return this.monsters.filter(monster => { 
          return monster.name.toLowerCase().indexOf(this.filter.toLowerCase()) > -1 
       }) 
-    }, 
-    columnClassObject: function() { 
-      return { 
-        'three-column': !this.filter, 
-      } 
-    }, 
-    monsterListLength: function() { 
-      return Object.keys(this.monstersByLetter).length; 
-    } 
+    }
   }
 }
 </script>
 
 <style scoped>
-.monster-block {
-  margin-top: 1rem;
-}
+    .monster-table-header{
+        text-decoration: underline;
+        cursor: pointer;
+        vertical-align: baseline;
+    }
 
-
+    .monster-table-header-class{
+        vertical-align: baseline;
+    }
 </style>
 
