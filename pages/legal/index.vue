c3b838bc116111057e5f0b93684c4fb341d0fe41
eepMoody <moody.designer@gmail.com>
2019-10-24 20:14:09 -0400

    add every copyright notice

--- pages/legal/index.vue
+++ pages/legal/index.vue
@@ -38,12 +38,166 @@
   <li><b>Inability to Comply:</b> If it is impossible for You to comply with any of the terms of this License with respect to some or all of the Open Game Content due to statute, judicial order, or governmental regulation then You may not Use any Open Game Material so affected.</li>
   <li><b>Termination:</b> This License will terminate automatically if You fail to comply with all terms herein and fail to cure such breach within 30 days of becoming aware of the breach. All sublicenses shall survive the termination of this License.</li>
   <li><b>Reformation:</b> If any provision of this License is held to be unenforceable, such provision shall be reformed only to the extent necessary to make it enforceable.</li>
-  <li><b>COPYRIGHT NOTICE</b> Open Game License v 1.0a Copyright 2000, Wizards of the Coast, LLC.</li>
-  </ol>
-  <hr/>
+  <li>
+  <p><b>COPYRIGHT NOTICE</b> Open Game License v 1.0a Copyright 2000, Wizards of the Coast, LLC.</p>
   <p>System Reference Document 5.1 Copyright 2016, Wizards of the Coast, Inc.; Authors Mike Mearls, Jeremy Crawford, Chris Perkins, Rodney Thompson, Peter Lee, James Wyatt, Robert J. Schwalb, Bruce R. Cordell, Chris Sims, and Steve Townshend, based on original material by E. Gary Gygax and Dave Arneson.</p>
+  <!-- <p>12 Perilous Towers © 2018 Open Design LLC; Authors: Jeff Lee.</p>
+<p>A Leeward Shore Author: Mike Welham. © 2018 Open Design LLC.</p>
+<p>A Night at the Seven Steeds. Author: Jon Sawatsky. © 2018 Open Design.</p>
+<p>Advanced Bestiary, Copyright 2004, Green Ronin Publishing, LLC; Author Matthew Sernett.</p>
+<p>Advanced Races: Aasimar. © 2014 Open Design; Author: Adam Roy.KoboldPress.com</p>
+<p>Advanced Races: Centaurs. © 2014 Open Design; Author: Karen McDonald. KoboldPress.com</p>
+<p>Advanced Races: Dragonkin © 2013 Open Design; Authors: Amanda Hamon Kunz.</p>
+<p>Advanced Races: Gearforged. © 2013 Open Design; Authors: Thomas Benton.</p>
+<p>Advanced Races: Gnolls. © 2015 Open Design; Authors: Ben McFarland and Brian Suskind.</p>
+<p>Advanced Races: Kobolds © 2013 Open Design; Authors: Nicholas Milasich, Matt Blackie.</p>
+<p>Advanced Races: Lizardfolk. © 2015 Open Design; Authors: Steven T.Helt, Stephen Rowe, and Dan Dillon.</p>
+<p>Advanced Races: Ravenfolk © 2014 Open Design; Authors: Wade Rockett.</p>
+<p>Advanced Races: Shadow Fey. © 2014 Open Design; Authors: Carlos and Holly Ovalle.</p>
+<p>Advanced Races: Trollkin. © 2015 Open Design; Authors: Steven T.Helt, Stephen Rowe, and Dan Dillon.</p>
+<p>Advanced Races: Werelions. © 2015 Open Design; Authors: Ben McFarland and Brian Suskind.</p>
+<p>An Enigma Lost in a Maze ©2018 Open Design. Author: Richard Pett.</p>
+<p>Bastion of Rime and Salt. Author: Jon Sawatsky. © 2018 Open Design LLC.</p>
+<p>Beyond Damage Dice © 2016 Open Design; Author: James J. Haeck.</p>
+<p>Birds of a Feather Author Kelly Pawlik. © 2019 Open Design LLC.</p>
+<p>Black Sarcophagus Author: Chris Lockey. © 2018 Open Design LLC.</p>
+<p>Blood Vaults of Sister Alkava. © 2016 Open Design. Author: Bill Slavicsek.</p>
+<p>Book of Lairs for Fifth Edition. Copyright 2016, Open Design; Authors Robert Adducci, Wolfgang Baur, Enrique Bertran, Brian Engard, Jeff Grubb, James J. Haeck, Shawn Merwin, Marc Radle, Jon Sawatsky, Mike Shea, Mike Welham, and Steve Winter.</p>
+<p>Cat and Mouse © 2015 Open Design; Authors: Richard Pett with Greg Marks.</p>
+<p>Courts of the Shadow Fey © 2019 Open Design LLC; Authors: Wolfgang Baur & Dan Dillon.</p> -->
+<p>Creature Codex. © 2018 Open Design LLC; Authors Wolfgang Baur, Dan Dillon, Richard Green, James Haeck, Chris Harris, Jeremy Hochhalter, James Introcaso, Chris Lockey, Shawn Merwin, and Jon Sawatsky.</p>
+<!-- <p>Creature Codex Lairs. © 2018 Open Design LLC; Author Shawn Merwin.</p>
+<p>Deep Magic: Angelic Seals and Wards © 2016 Open Design; Author: Dan Dillon.</p>
+<p>Deep Magic: Battle Magic © 2016 Open Design; Author: Greg Marks.</p>
+<p>Deep Magic: Blood and Doom © 2017 Open Design; Author: Chris Harris.</p>
+<p>Deep Magic: Chaos Magic © 2016 Open Design; Author: Greg Marks.</p>
+<p>Deep Magic: Clockwork © 2016 Open Design; Author: Scott Carter.</p>
+<p>Deep Magic: Combat Divination © 2019 Open Design LLC; Author: Matt Corley.</p>
+<p>Deep Magic: Dragon Magic © 2017 Open Design; Author: Shawn Merwin.</p>
+<p>Deep Magic: Elemental Magic © 2017 Open Design; Author: Dan Dillon.</p>
+<p>Deep Magic: Elven High Magic © 2016 Open Design; Author: Greg Marks.</p>
+<p>Deep Magic: Hieroglyph Magic © 2018 Open Design LLC; Author: Michael Ohl.</p>
+<p>Deep Magic: Illumination Magic © 2016 Open Design; Author: Greg Marks..</p>
+<p>Deep Magic: Ley Line Magic © 2016 Open Design; Author: Dan Dillon.</p>
+<p>Deep Magic: Mythos Magic © 2018 Open Design LLC; Author: Christopher Lockey.</p>
+<p>Deep Magic: Ring Magic © 2016 Open Design; Author: Dan Dillon.</p>
+<p>Deep Magic: Runes © 2016 Open Design; Author: Chris Harris.</p>
+<p>Deep Magic: Shadow Magic © 2016 Open Design; Author: Michael Ohl</p>
+<p>Deep Magic: Time Magic © 2018 Open Design LLC; Author: Carlos Ovalle.</p>
+<p>Deep Magic: Void Magic © 2016 Open Design; Author: Dan Dillon.</p>
+<p>Deep Magic: Winter © 2019 Open Design LLC; Author: Mike Welham.</p>
+<p>Demon Cults & Secret Societies for 5th Edition. Copyright 2017 Open Design. Authors: Jeff Lee, Mike Welham, Jon Sawatsky.</p>
+<p>Divine Favor: the Cleric. Author: Stefen Styrsky Copyright 2011, Open Design LLC, .</p>
+<p>Divine Favor: the Druid. Author: Stefen Styrsky ©2011, Open Design LLC.</p>
+<p>Divine Favor: the Inquisitor. Author: Stefen Styrsky ©2011, Open Design LLC.</p>
+<p>Divine Favor: the Oracle. Author: Stefen Styrsky ©2011, Open Design LLC.</p>
+<p>Divine Favor: the Paladin. Author: Stefen Styrsky ©2011, Open Design LLC.</p>
+<p>Eldritch Lairs for Fifth Edition. Copyright 2018, Open Design; Authors James J. Haeck, Jerry LeNeave, Mike Shea, Bill Slavicsek.</p>
+<p>Empire of the Ghouls, ©2007 Wolfgang Baur, www.wolfgangbaur.com. All rights reserved.</p>
+<p>Fifth Edition Foes, © 2015, Necromancer Games, Inc.; Authors Scott Greene, Matt Finch, Casey Christofferson, Erica Balsley, Clark Peterson, Bill Webb, Skeeter Green, Patrick Lawinger, Lance Hawvermale, Scott Wylie Roberts “Myrystyr”, Mark R. Shipley, “Chgowiz”</p>
+<p>Firefalls of Ghoss. Author: Jon Sawatsky. © 2018 Open Design LLC.</p>
+<p>Fowl Play. Author: Kelly Pawlik. © 2019 Open Design LLC.</p>
+<p>Grimalkin ©2016 Open Design; Authors: Richard Pett with Greg Marks</p>
+<p>Imperial Gazetteer, ©2010, Open Design LLC.</p>
+<p>Items Wondrous Strange © 2017 Open Design; Authors: James Bitoy, Peter von Bleichert, Dan Dillon, James Haeck, Neal Litherland, Adam Roy, and Jon Sawatsky.</p>
+<p>Kobold Quarterly issue 21,Copyright 2012, Open Design LLC.</p>
+<p>KPOGL Wiki https:kpogl.wikidot.com/</p>
+<p>Last Gasp © 2016 Open Design; Authors: Dan Dillon.</p>
+<p>Mad Maze of the Moon Kingdom, Copyright 2018 Open Design LLC. Author: Richard Green.</p>
+<p>Margreve Player’s Guide © 2019 Open Design LLC; Authors: Dan Dillon, Dennis Sustare, Jon Sawatsky, Lou Anders, Matthew Corley, and Mike Welham.</p>
+<p>Midgard Bestiary for Pathfinder Roleplaying Game, © 2012 Open Design LLC; Authors: Adam Daigle with Chris Harris, Michael Kortes, James MacKenzie, Rob Manning, Ben McFarland, Carlos Ovalle, Jan Rodewald, Adam Roy, Christina Stiles, James Thomas, and Mike Welham.</p>
+<p>Midgard Campaign Setting © 2012 Open Design LLC. Authors: Wolfgang Baur, Brandon Hodge, Christina Stiles, Dan Voyce, and Jeff Grubb.</p>
+<p>Midgard Heroes © 2015 Open Design; Author: Dan Dillon.</p>
+<p>Midgard Heroes Handbook © 2018 Open Design LLC; Authors: Chris Harris, Dan Dillon, Greg Marks, Jon Sawatsky, Michael Ohl, Richard Green, Rich Howard, Scott Carter, Shawn Merwin, and Wolfgang Baur.</p>
+<p>Midgard Sagas ©2018 Open Design LLC; Authors: Wolfgang Baur, Dan Dillon, Robert Fairbanks, Greg Marks, Ben McFarland, Kelly Pawlik, Brian Suskind, and Troy Taylor.</p>
+<p>Midgard Worldbook. Copyright ©2018 Open Design LLC. Authors: Wolfgang Baur, Dan Dillon, Richard Green, Jeff Grubb, Chris Harris, Brian Suskind, and Jon Sawatsky.</p>
+<p>Monkey Business Author: Richard Pett. © 2018 Open Design LLC.</p>
+<p>Monte Cook’s Arcana Evolved Copyright 2005 Monte J. Cook. All rights reserved.</p>
+<p>New Paths: The Expanded Shaman Copyright 2012, Open Design LLC.; Author: Marc Radle.</p>
+<p>Northlands © 2011, Open Design LL C; Author: Dan Voyce; www.koboldpress.com.</p>
+<p>Pathfinder Advanced Players Guide. Copyright 2010, Paizo Publishing, LLC; Author: Jason Bulmahn.</p>
+<p>Pathfinder Roleplaying Game Advanced Race Guide © 2012, Paizo Publishing, LLC; Authors: Dennis Baker, Jesse Benner, Benjamin Bruck, Jason Bulmahn, Adam Daigle, Jim Groves, Tim Hitchcock, Hal MacLean, Jason Nelson, Stephen Radney-MacFarland, Owen K.C. Stephens, Todd Stewart, and Russ Taylor.</p>
+<p>Pathfinder Roleplaying Game Bestiary, © 2009, Paizo Publishing, LLC; Author Jason Bulmahn, based on material by Jonathan Tweet, Monte Cook, and Skip Williams.</p>
+<p>Pathfinder Roleplaying Game Bestiary 2, © 2010, Paizo Publishing, LLC; Authors Wolfgang Baur, Jason Bulmahn, Adam Daigle, Graeme Davis, Crystal Frasier, Joshua J. Frost, Tim Hitchcock, Brandon Hodge, James Jacobs, Steve Kenson, Hal MacLean, Martin Mason, Rob McCreary, Erik Mona, Jason Nelson, Patrick Renie, Sean K Reynolds, F. Wesley Schneider, Owen K.C. Stephens, James L. Sutter, Russ Taylor, and Greg A. Vaughan, based on material by Jonathan Tweet, Monte Cook, and Skip Williams.</p>
+<p>Pathfinder Roleplaying Game Bestiary 3, © 2011, Paizo Publishing, LLC; Authors Jesse Benner, Jason Bulmahn, Adam Daigle, James Jacobs, Michael Kenway, Rob McCreary, Patrick Renie, Chris Sims, F. Wesley Schneider, James L. Sutter, and Russ Taylor, based on material by Jonathan Tweet, Monte Cook, and Skip Williams. Pathfinder Roleplaying Game Ultimate Combat. © 2011, Paizo Publishing, LLC; Authors: Jason Bulmahn, Tim Hitchcock, Colin McComb, Rob McCreary, Jason Nelson, Stephen Radney-MacFarland, Sean K Reynolds, Owen K.C. Stephens, and Russ Taylor</p>
+<p>Pathfinder Roleplaying Game: Ultimate Equipment Copyright 2012, Paizo Publishing, LLC; Authors: Dennis Baker, Jesse Benner, Benjamin Bruck, Ross Byers, Brian J. Cortijo, Ryan Costello, Mike Ferguson, Matt Goetz, Jim Groves, Tracy Hurley, Matt James, Jonathan H. Keith, Michael Kenway, Hal MacLean, Jason Nelson, Tork Shaw, Owen K C Stephens, Russ Taylor, and numerous RPG Superstar contributors</p>
+<p>Pathfinder RPG Core Rulebook Copyright 2009, Paizo Publishing, LLC; Author: Jason Bulmahn, based on material by Jonathan Tweet, Monte Cook, and Skip Williams.</p>
+<p>Pathfinder Ultimate Magic Copyright 2011, Paizo Publishing, LLC; Author: Jason Bulmahn.</p>
+<p>Prepared: A Dozen Adventures for Fifth Edition. Copyright 2016, Open Design; Author Jon Sawatsky.</p>
+<p>Prepared 2: A Dozen Fifth Edition One-Shot Adventures. Copyright 2017, Open Design; Author Jon Sawatsky.</p>
+<p>Pride of the Mushroom Queen. Author: Mike Welham. © 2018 Open Design LLC.</p>
+<p>Reclamation of Hallowhall. Author: Jeff Lee. © 2019 Open Design LLC.</p>
+<p>Red Lenny’s Famous Meat Pies. Author: James J. Haeck. © 2017 Open Design.</p>
+<p>Return to Castle Shadowcrag. © 2018 Open Design; Authors Wolfgang Baur, Chris Harris, and Thomas Knauss.</p>
+<p>Rumble in the Henhouse. © 2019 Open Design LLC. Author Kelly Pawlik.</p>
+<p>Run Like Hell. ©2019 Open Design LLC. Author Mike Welham.</p>
+<p>Sanctuary of Belches © 2016 Open Design; Author: Jon Sawatsky.</p>
+<p>Shadow’s Envy. Author: Mike Welham. © 2018 Open Design LLC.</p>
+<p>Shadows of the Dusk Queen, © 2018, Open Design LLC; Author Marc Radle.</p>
+<p>Skeletons of the Illyrian Fleet Author: James J. Haeck. © 2018 Open Design LLC.</p>
+<p>Smuggler's Run Author: Mike Welham. © 2018 Open Design LLC.</p>
+<p>Southlands Heroes © 2015 Open Design; Author: Rich Howard.</p>
+<p>Spelldrinker’s Cavern. Author: James J. Haeck. © 2017 Open Design.</p>
+<p>Steam & Brass © 2006, Wolfgang Baur, www.wolfgangbaur.com.</p>
+<p>Streets of Zobeck. © 2011, Open Design LLC. Authors: Ben McFarland, Mike Franke, Richard Pett, Christina Stiles, Matthew Stinson.</p>
+<p>Streets of Zobeck for 5th Edition. Copyright 2017, Open Design; Authors Ben McFarland, Mike Franke, Richard Pett, Christina Stiles, and Matthew Stinson. Converted for the 5th Edition of Dungeons & Dragons by Chris Harris</p>
+<p>Storming the Queen’s Desire Author: Mike Welham. © 2018 Open Design LLC.</p>
+<p>Sunken Empires ©2010, Open Design, LL C; Authors: Brandon Hodge, David “Zeb” Cook, and Stefen Styrsky.</p>
+<p>System Reference Document Copyright 2000. Wizards of the Coast, Inc; Authors Jonathan Tweet, Monte Cook, Skip Williams, based on material by E. Gary Gygax and Dave Arneson.</p>
+<p>System Reference Document 5.0 Copyright 2016, Wizards of the Coast, Inc.; Authors Mike Mearls, Jeremy Crawford, Chris Perkins, Rodney Thompson, Peter Lee, James Wyatt, Robert J. Schwalb, Bruce R. Cordell, Chris Sims, and Steve Townshend, based on original material by E. Gary Gygax and Dave Arneson.</p>
+<p>System Reference Document 5.1 Copyright 2016, Wizards of the Coast, Inc.; Authors Mike Mearls, Jeremy Crawford, Chris Perkins, Rodney Thompson, Peter Lee, James Wyatt, Robert J. Schwalb, Bruce R. Cordell, Chris Sims, and Steve Townshend, based on original material by E. Gary Gygax and Dave Arneson.</p>
+<p>Tales of the Old Margreve © 2019 Open Design LLC; Matthew Corley, Wolfgang Baur, Richard Green, James Introcaso, Ben McFarland, and Jon Sawatsky.</p>
+<p>Tales of Zobeck, ©2008, Open Design LLC. Authors: Wolfgang Baur, Bill Collins, Tim and Eileen Connors, Ed Greenwood, Jim Groves, Mike McArtor, Ben McFarland, Joshua Stevens, Dan Voyce.</p>
+<p>The Bagiennik Game. Author: Jon Sawatsky. © 2018 Open Design LLC.</p>
+<p>The Beacon at the Top of the World. Author: Mike Welham. © 2019 Open Design LLC.</p>
+<p>The Book of Eldritch Might, Copyright 2004 Monte J. Cook. All rights reserved.</p>
+<p>The Book of Experimental Might Copyright 2008, Monte J. Cook. All rights reserved.</p>
+<p>The Book of Fiends, © 2003, Green Ronin Publishing; Authors Aaron Loeb, Erik Mona, Chris Pramas, Robert J. Schwalb.</p>
+<p>The Clattering Keep. Author: Jon Sawatsky. © 2017 Open Design.</p>
+<p>The Empty Village Author Mike Welham. © 2018 Open Design LLC.</p>
+<p>The Infernal Salt Pits. Author: Richard Green. © 2018 Open Design LLC.</p>
+<p>The Lamassu’s Secrets, Copyright 2018 Open Design LLC. Author: Richard Green.</p>
+<p>The Lost Temple of Anax Apogeion. Author: Mike Shea, AKA Sly Flourish. © 2018 Open Design LLC.</p>
+<p>The Raven’s Call. Copyright 2013, Open Design LLC. Author: Wolfgang Baur.</p>
+<p>The Raven’s Call 5th Edition © 2015 Open Design; Authors: Wolfgang Baur and Dan Dillon.</p>
+<p>The Returners’ Tower. Author: Jon Sawatsky. © 2018 Open Design LLC.</p>
+<p>The Rune Crypt of Sianis. Author: Jon Sawatsky. © 2018 Open Design LLC.</p>
+<p>The Scorpion’s Shadow. Author: Chris Harris. © 2018 Open Design LLC.</p>
+<p>The Seal of Rhydaas. Author: James J. Haeck. © 2017 Open Design.</p>
+<p>The Sunken Library of Qezzit Qire. © 2019 Open Design LLC. Author Mike Welham.</p>
+<p>The Tomb of Mercy (C) 2016 Open Design. Author: Sersa Victory.</p>
+<p>The Wilding Call Author Mike Welham. © 2019 Open Design LLC.</p>
+<p>Three Little Pigs - Part One: Nulah’s Tale. Author: Richard Pett. © 2019 Open Design LLC.</p>
+<p>Three Little Pigs - Part Two: Armina’s Peril. Author: Richard Pett. © 2019 Open Design LLC.</p>
+<p>Three Little Pigs - Part Three: Madgit’s Story. Author: Richard Pett. © 2019 Open Design LLC.</p>
+<p>Tomb of Tiberesh © 2015 Open Design; Author: Jerry LeNeave.</p> -->
+<p>Tome of Beasts. Copyright 2016, Open Design; Authors Chris Harris, Dan Dillon, Rodrigo Garcia Carmona, and Wolfgang Baur.</p>
+<!-- <p>Tome of Horrors. Copyright 2002, Necromancer Games, Inc.; Authors: Scott Greene, with Clark Peterson, Erica Balsley, Kevin Baase, Casey Christofferson, Lance Hawvermale, Travis Hawvermale, Patrick Lawinger, and Bill Webb; Based on original content from TSR.</p>
+<p>Unlikely Heroes for 5th Edition © 2016 Open Design; Author: Dan Dillon.</p>
+<p>Wrath of the Bramble King Author: Mike Welham. © 2018 Open Design LLC.</p>
+<p>Wrath of the River King © 2017 Open Design; Author: Wolfgang Baur and Robert Fairbanks</p>
+<p>Warlock Bestiary Authors: Jeff Lee with Chris Harris, James Introcaso, and Wolfgang Baur. © 2018 Open Design LLC.</p>
+<p>Warlock Guide to the Shadow Realms. Author: Kelly Pawlik. © 2019 Open Design LLC.</p>
+<p>Warlock Part 1. Authors: Wolfgang Baur, Dan Dillon, Troy E. Taylor, Ben McFarland, Richard Green. © 2017 Open Design.</p>
+<p>Warlock 2: Dread Magic. Authors: Wolfgang Baur, Dan Dillon, Jon Sawatsky, Richard Green. © 2017 Open Design.</p>
+<p>Warlock 3: Undercity. Authors: James J. Haeck, Ben McFarland, Brian Suskind, Peter von Bleichert, Shawn Merwin. © 2018 Open Design.</p>
+<p>Warlock 4: The Dragon Empire. Authors: Wolfgang Baur, Chris Harris, James J. Haeck, Jon Sawatsky, Jeremy Hochhalter, Brian Suskind. © 2018 Open Design.</p>
+<p>Warlock 5: Rogue’s Gallery. Authors: James J. Haeck, Shawn Merwin, Richard Pett. © 2018 Open Design.</p>
+<p>Warlock 6: City of Brass. Authors: Richard Green, Jeff Grubb, Richard Pett, Steve Winter. © 2018 Open Design.</p>
+<p>Warlock 7: Fey Courts. Authors: Wolfgang Baur, Shawn Merwin , Jon Sawatsky, Troy E. Taylor. © 2018 Open Design.</p>
+<p>Warlock 8: Undead. Authors: Wolfgang Baur, Dan Dillon, Chris Harris, Kelly Pawlik. © 2018 Open Design.</p>
+<p>Warlock 9: The World Tree. Authors: Wolfgang Baur, Sarah Madsen, Richard Green, and Kelly Pawlik. © 2018 Open Design LLC.</p>
+<p>Warlock 10: The Magocracies. Authors: Dan Dillon, Ben McFarland, Kelly Pawlik, Troy E. Taylor. © 2019 Open Design LLC.</p>
+<p>Warlock 11: Treasure Vaults. Authors: Lysa Chen, Richard Pett, Marc Radle, Mike Welham. © 2019 Open Design LLC.</p>
+<p>Warlock 12: Dwarves. Authors: Wolfgang Baur, Ben McFarland and Robert Fairbanks, Hannah Rose, Ashley Warren. © 2019 Open Design LLC.</p>
+<p>Warlock 13: War & Battle Authors: Kelly Pawlik and Brian Suskind. © 2019 Open Design LLC.</p>
+<p>Zobeck Gazetteer, ©2008, Open Design LLC; Author: Wolfgang Baur.</p>
+<p>Zobeck Gazetteer Volume 2: Dwarves of the Ironcrags ©2009, Open Design LLC.</p>
+<p>Zobeck Gazetteer for 5th Edition. Copyright ©2018 Open Design LLC. Author: James Haeck.</p>
+<p>Zobeck Gazetteer for the Pathfinder Roleplaying Game, ©2012, Open Design LLC. Authors: Wolfgang Baur and Christina Stiles</p> -->
   <p>ope5e.com v2.0, copyright 2019.</p>
-
+  </li>
+  </ol>
   <p>END OF LICENSE</p>
 </section>
 
