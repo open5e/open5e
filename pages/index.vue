196b3fbdd7fcb6b8f5916bc7c6b7341f550a96c1
Ean Moody <moody.designer@gmail.com>
2023-05-20 17:38:31 -0400

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="container docs-container">
     <h1>Welcome to Open5e!</h1>
-    <p>Open5e aims to be the best open-source resource for 5e content.</p>
+    <p>The best open-source resource for 5e content.</p>
 
     <h2>Projects we love</h2>
     <h3>Slyflourish A.K.A. "The Lazy DM"</h3>
@@ -9,11 +9,15 @@
       <a href="https://slyflourish.com/lazydm/">The Lazy Dungeon Master</a> is a
       masterwork of a guide for anyone aspiring to run Dungeons & Dragons or any
       RPG. Check out
-      <a href="https://slyflourish.com/top_advice.html">How to Be a Great D&D Dungeon Master</a>, even if you're an expert. I can guarantee you'll learn something.
+      <a href="https://slyflourish.com/top_advice.html"
+        >How to Be a Great D&D Dungeon Master</a
+      >, even if you're an expert. I can guarantee you'll learn something.
     </p>
     <p>
       I'd also like to recommend
-      <a href="https://slyflourish.com/playing_dnd_can_save_your_life.html">Playing D&D Can Save Your Life</a>, an inspiring take on the role of fantasy and play in mental health.
+      <a href="https://slyflourish.com/playing_dnd_can_save_your_life.html"
+        >Playing D&D Can Save Your Life</a
+      >, an inspiring take on the role of fantasy and play in mental health.
     </p>
 
     <h3>LegendKeeper</h3>
@@ -40,14 +44,12 @@
       <b>VueJs UI layer.</b> A Vue.js single-page app, using the Nuxt framework.
     </p>
 
-    <a
-      href="https://github.com/open5e/open5e"
-      class="external-button"
-    ><img src="/img/github-website-button.png"></a>
-    <a
-      href="https://github.com/open5e/open5e-api"
-      class="external-button"
-    ><img src="/img/github-api-button.png"></a>
+    <a href="https://github.com/open5e/open5e" class="external-button"
+      ><img src="/img/github-website-button.png"
+    /></a>
+    <a href="https://github.com/open5e/open5e-api" class="external-button"
+      ><img src="/img/github-api-button.png"
+    /></a>
 
     <h3>Support us on Patreon</h3>
     <p>
@@ -55,20 +57,18 @@
       on donations and patrons to pay for our server costs, and to fund an
       increasing effort to fill out flavor text and add art.
     </p>
-    <a
-      href="https://www.patreon.com/open5e"
-      class="external-button"
-    ><img src="/img/patron-button.png"></a>
+    <a href="https://www.patreon.com/open5e" class="external-button"
+      ><img src="/img/patron-button.png"
+    /></a>
 
     <h3>Join the Discord</h3>
     <p>
       Join us to chat, talk about development, and share neat homebrew. We don't
       bite!
     </p>
-    <a
-      href="https://discord.gg/QXqF6gSVqB"
-      class="external-button"
-    ><img src="/img/discord-button.png"></a>
+    <a href="https://discord.gg/QXqF6gSVqB" class="external-button"
+      ><img src="/img/discord-button.png"
+    /></a>
 
     <h2>The API</h2>
     <div class="side-note">
