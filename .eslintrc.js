8b815d59e75adb70f7f783dc9b1c6dd4141225be
Ean Moody <moody.designer@gmail.com>
2023-05-20 16:13:24 -0400

    add vue eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,22 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
+    'plugin:vue/essential',
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue'],
   // add your custom rules here
   rules: {
-    "eqeqeq": "off",
-    "curly": "error",
-    "quotes": ["error", "single"]
-  }
-}
+    eqeqeq: 'off',
+    curly: 'error',
+    quotes: ['error', 'single'],
+  },
+};
