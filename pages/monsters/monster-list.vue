ef334b4042622a91517242e419142eead65d21a3
Ean Moody <moody.designer@gmail.com>
2023-05-20 17:44:21 -0400

    test

--- pages/monsters/monster-list.vue
+++ pages/monsters/monster-list.vue
@@ -2,69 +2,39 @@
   <section class="container docs-container">
     <h2 class="filter-header">
       <span>Monster List</span>
-      <filter-input
-        placeholder="Filter monsters..."
-        @input="updateFilter"
-      />
+      <filter-input placeholder="Filter monsters" @input="updateFilter" />
     </h2>
     <div>
-      <p v-if="!monstersList.length">
-        Loading...
-      </p>
-      <table
-        v-else
-        class="fiterable-table"
-      >
+      <p v-if="!monstersList.length">Loading...</p>
+      <table v-else class="fiterable-table">
         <thead>
           <tr>
-            <th
-              class="monster-table-header"
-              @click="sort('name')"
-            >
-              Name
-            </th>
-            <th
-              class="monster-table-header"
-              @click="sort('type')"
-            >
-              Type
-            </th>
-            <th
-              class="monster-table-header"
-              @click="sort('challenge_rating')"
-            >
+            <th class="monster-table-header" @click="sort('name')">Name</th>
+            <th class="monster-table-header" @click="sort('type')">Type</th>
+            <th class="monster-table-header" @click="sort('challenge_rating')">
               CR
             </th>
-            <th
-              class="monster-table-header"
-              @click="sort('size')"
-            >
-              Size
-            </th>
-            <th
-              class="monster-table-header"
-              @click="sort('hit_points')"
-            >
+            <th class="monster-table-header" @click="sort('size')">Size</th>
+            <th class="monster-table-header" @click="sort('hit_points')">
               Hit Points
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr
-            v-for="monster in monstersListed"
-            :key="monster.slug"
-          >
+          <tr v-for="monster in monstersListed" :key="monster.slug">
             <td>
               <nuxt-link
                 tag="a"
                 :params="{ id: monster.slug }"
                 :to="`/monsters/${monster.slug}`"
               >
-                {{ monster.name
-                }}
+                {{ monster.name }}
               </nuxt-link>
               <source-tag
-                v-if="monster.document__slug && monster.document__slug !== 'wotc-srd'"
+                v-if="
+                  monster.document__slug &&
+                  monster.document__slug !== 'wotc-srd'
+                "
                 class=""
                 :title="monster.document__title"
                 :text="monster.document__slug"
@@ -78,67 +48,73 @@
         </tbody>
       </table>
     </div>
-    <span style="display:none;">Sorting by sort={{ currentSortProperty }}, dir={{ currentSortDir }}</span>
+    <span style="display: none"
+      >Sorting by sort={{ currentSortProperty }}, dir={{ currentSortDir }}</span
+    >
   </section>
 </template>
 
 <script>
-import FilterInput from '~/components/FilterInput.vue'
-import FractionRenderer from '~/components/FractionRenderer.vue'
-import SourceTag from '~/components/SourceTag.vue'
-import { useMainStore } from '~/store'
+import FilterInput from '~/components/FilterInput.vue';
+import FractionRenderer from '~/components/FractionRenderer.vue';
+import SourceTag from '~/components/SourceTag.vue';
+import { useMainStore } from '~/store';
 
 export default {
   components: {
     FilterInput,
     FractionRenderer,
-    SourceTag
+    SourceTag,
   },
   setup() {
-    const store = useMainStore()
-    return { store }
+    const store = useMainStore();
+    return { store };
   },
   data() {
     return {
       filter: '',
       currentSortProperty: 'challenge_rating',
-      currentSortDir: 'asc'
-    }
+      currentSortDir: 'asc',
+    };
   },
   computed: {
     monstersList() {
-      return this.store.allMonsters
+      return this.store.allMonsters;
     },
     monstersListed: {
       get: function () {
-        this.filteredMonsters.forEach(monster => monster.challenge_rating = eval(monster.challenge_rating));
-        return this.filteredMonsters
+        this.filteredMonsters.forEach(
+          (monster) =>
+            (monster.challenge_rating = eval(monster.challenge_rating))
+        );
+        return this.filteredMonsters;
       },
       set: function () {
         return this.filteredMonsters.sort((a, b) => {
           let modifier = 1;
           if (this.currentSortDir === 'desc') {
-            modifier = -1
+            modifier = -1;
           }
           if (a[this.currentSortProperty] < b[this.currentSortProperty]) {
-            return -1 * modifier
+            return -1 * modifier;
           }
           if (a[this.currentSortProperty] > b[this.currentSortProperty]) {
-            return 1 * modifier
+            return 1 * modifier;
           }
           return 0;
         });
-      }
+      },
     },
     filteredMonsters: function () {
-      return this.monstersList
-        .filter(monster => {
-          return monster.name.toLowerCase().indexOf(this.filter.toLowerCase()) > -1
-        })
-    }
+      return this.monstersList.filter((monster) => {
+        return (
+          monster.name.toLowerCase().indexOf(this.filter.toLowerCase()) > -1
+        );
+      });
+    },
   },
   beforeCreate() {
-    this.store.loadMonsterList()
+    this.store.loadMonsterList();
   },
   methods: {
     updateFilter: function (val) {
@@ -153,9 +129,9 @@ export default {
       }
       this.currentSortProperty = prop;
       this.monstersListed = {};
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -169,4 +145,3 @@ export default {
   vertical-align: baseline;
 }
 </style>
-
