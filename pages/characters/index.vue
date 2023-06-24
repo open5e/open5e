35d11d0bf3104eb5b83bff2107b4192080bb8dff
calumbell <calumabell@googlemail.com>
2023-06-24 16:44:52 +0100

    removed races from /characters route

--- pages/characters/index.vue
+++ pages/characters/index.vue
@@ -8,38 +8,6 @@
             {{ section.name }}
           </nuxt-link>
         </li>
-        <li>
-          Races
-          <ul>
-            <li>
-              <nuxt-link tag="a" to="/races/tiefling"> Tiefling </nuxt-link>
-            </li>
-            <li>
-              <nuxt-link tag="a" to="/races/dragonborn"> Dragonborn </nuxt-link>
-            </li>
-            <li>
-              <nuxt-link tag="a" to="/races/dwarf"> Dwarf </nuxt-link>
-            </li>
-            <li>
-              <nuxt-link tag="a" to="/races/elf"> Elf </nuxt-link>
-            </li>
-            <li>
-              <nuxt-link tag="a" to="/races/gnome"> Gnome </nuxt-link>
-            </li>
-            <li>
-              <nuxt-link tag="a" to="/races/half-elf"> Half-Elf </nuxt-link>
-            </li>
-            <li>
-              <nuxt-link tag="a" to="/races/half-orc"> Half-Orc </nuxt-link>
-            </li>
-            <li>
-              <nuxt-link tag="a" to="/races/halfling"> Halfling </nuxt-link>
-            </li>
-            <li>
-              <nuxt-link tag="a" to="/races/human"> Human </nuxt-link>
-            </li>
-          </ul>
-        </li>
       </ul>
     </div>
   </section>
@@ -87,5 +55,3 @@ export default {
   },
 };
 </script>
-
-<style></style>
