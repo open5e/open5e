620214209192ab377d66dba3130af916d338a990
calumbell <calumabell@googlemail.com>
2023-06-25 15:52:12 +0100

    Populated classes/[className] route w/ data from api

--- pages/classes/[className].vue
+++ pages/classes/[className].vue
@@ -1,11 +1,64 @@
 <template>
-  <section class="container docs-container">
-    <h1>{{ className.charAt(0).toUpperCase() + className.slice(1) }}</h1>
-    <p v-if="loading">Loading...</p>
-    <div v-else>
+  <main v-if="!loading" class="container docs-container">
+    <h1>{{ className }}</h1>
+
+    <section>
+      <h2>Class Features</h2>
+      <p>As a {{ className }} you gain the following features.</p>
+      <h3>Hit Points</h3>
+      <p>
+        <span class="font-bold">Hit Dice: </span>
+        {{ classDetails.hit_dice }} per {{ className }} level
+      </p>
+      <p>
+        <span class="font-bold">Hit Points at 1st Level: </span>
+        {{ classDetails.hp_at_1st_level }}
+      </p>
+      <p>
+        <span class="font-bold">Hit Points at Higher Levels: </span>
+        {{ classDetails.hp_at_higher_levels }}
+      </p>
+
+      <h3>Proficiencies</h3>
+      <p>
+        <span class="font-bold">Armor: </span>
+        {{ classDetails.prof_armor }}
+      </p>
+      <p>
+        <span class="font-bold">Weapons: </span>
+        {{ classDetails.prof_weapons }}
+      </p>
+      <p>
+        <span class="font-bold">Tools: </span>
+        {{ classDetails.prof_tools }}
+      </p>
+      <p>
+        <span class="font-bold">Saving Throws: </span>
+        {{ classDetails.prof_saving_throws }}
+      </p>
+      <p>
+        <span class="font-bold">Skills: </span>
+        {{ classDetails.prof_skills }}
+      </p>
+
+      <h3>The {{ className }}</h3>
+      <md-viewer :text="classDetails.table" />
+    </section>
+
+    <section>
+      <h2>Class Abilities</h2>
       <md-viewer :text="classDetails.desc" />
-    </div>
-  </section>
+    </section>
+
+    <section>
+      <h2>{{ className }} {{ classDetails.subtypes_name }}</h2>
+      <ul v-for="archetype in classDetails.archetypes" :key="archetype">
+        <li>{{ archetype.name }}</li>
+      </ul>
+    </section>
+  </main>
+
+  <p v-else>Loading...</p>
 </template>
 
 <script>
@@ -17,7 +70,7 @@ export default {
   data() {
     return {
       loading: false,
-      className: this.$route.params.className,
+      className: '',
       classDetails: null,
     };
   },
@@ -37,11 +90,10 @@ export default {
       const url = `${useRuntimeConfig().public.apiUrl}/classes/`;
       axios.get(url + this.$route.params.className).then((res) => {
         this.classDetails = res.data;
+        this.className = res.data.name;
         this.loading = false;
       });
     },
   },
 };
 </script>
-
-<style></style>
