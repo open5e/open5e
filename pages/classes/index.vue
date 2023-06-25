3bfaab32a782a8c5ed3e6648ccaaa2d27ea1f44f
calumbell <calumabell@googlemail.com>
2023-06-25 12:18:45 +0100

    class list is now pulled from the api (via pinia store)

--- pages/classes/index.vue
+++ pages/classes/index.vue
@@ -3,41 +3,10 @@
     <h1>Classes</h1>
     <div class="docs-toc">
       <ul>
-        <li>
-          <nuxt-link tag="a" to="/classes/barbarian"> Barbarian </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link tag="a" to="/classes/bard"> Bard </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link tag="a" to="/classes/cleric"> Cleric </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link tag="a" to="/classes/druid"> Druid </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link tag="a" to="/classes/fighter"> Fighter </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link tag="a" to="/classes/monk"> Monk </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link tag="a" to="/classes/paladin"> Paladin </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link tag="a" to="/classes/ranger"> Ranger </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link tag="a" to="/classes/rogue"> Rogue </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link tag="a" to="/classes/sorcerer"> Sorcerer </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link tag="a" to="/classes/warlock"> Warlock </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link tag="a" to="/classes/wizard"> Wizard </nuxt-link>
+        <li v-for="charClass in classes" :key="charClass.slug">
+          <nuxt-link :to="`/classes/${charClass.slug}`">
+            {{ charClass.name }}
+          </nuxt-link>
         </li>
       </ul>
     </div>
@@ -45,7 +14,21 @@
 </template>
 
 <script>
-export default {};
+import { useMainStore } from '../../store/index';
+
+export default {
+  setup() {
+    const store = useMainStore();
+    store.loadClasses();
+    return { store };
+  },
+
+  computed: {
+    classes: function () {
+      return [...this.store.classes];
+    },
+  },
+};
 </script>
 
 <style></style>
