3addebc455f008d6f9d0574bf85d19a9d7a583d2
calumbell <calumabell@googlemail.com>
2023-07-03 22:10:41 +0100

    added implied pages/spells/by-class page

---
+++ pages/spells/by-class/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <main class="container">
+    <h1>Spells by Class</h1>
+    <ul class="docs-toc">
+      <li v-for="className in available_classes" :key="className">
+        <nuxt-link tag="a" :href="`/spells/by-class/${className}`">
+          {{ className }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      available_classes: [
+        'Bard',
+        'Cleric',
+        'Sorcerer',
+        'Wizard',
+        'Druid',
+        'Paladin',
+        'Warlock',
+      ],
+    };
+  },
+};
+</script>
+<style></style>
