1496dbc2192c9495a68f8f66ed687fe59ebbc9a7
calumbell <calumabell@googlemail.com>
2023-08-29 16:00:34 +0100

    removed console logs

--- pages/equipment/[section].vue
+++ pages/equipment/[section].vue
@@ -21,7 +21,6 @@ export default {
     return axios
       .get(url)
       .then((response) => {
-        console.log(response.status);
         this.section = response.data;
       })
       .catch(() => {
