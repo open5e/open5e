7afbd7f48bf42ff5232ad04f1eea67e42b19f5cc
calumbell <calumabell@googlemail.com>
2023-12-20 21:50:26 +0000

    refactored classes/[className]/[subclass] route w/ useFetchArticle

--- pages/classes/[className]/[subclass].vue
+++ pages/classes/[className]/[subclass].vue
@@ -15,33 +15,16 @@
   <p v-else>Loading...</p>
 </template>
 
-<script>
-import axios from 'axios';
-export default {
-  data() {
-    return { subclass: null };
-  },
-  mounted() {
-    const { className, subclass } = useRoute().params;
-    const url = `${useRuntimeConfig().public.apiUrl}/classes/${className}/`;
-
-    //you will need to enable CORS to make this work
-    return axios
-      .get(url)
-      .then((response) => {
-        this.subclass = response.data.archetypes.find(
-          (subclass) => subclass.slug === this.$route.params.subclass
-        );
-        if (!this.subclass) {
-          navigateTo(`/classes/${className}`, { statusCode: '404' });
-        }
-      })
-      .catch(() => {
-        throw showError({
-          statusCode: 404,
-          message: `${useRoute().path} does not exist`,
-        });
-      });
-  },
-};
+<script setup>
+const classData = await useFetchArticle({
+  slug: useRoute().params.className,
+  category: 'classes',
+});
+const slug = useRoute().params.subclass;
+const subclass = classData.archetypes.find(
+  (subclass) => subclass.slug === slug
+);
+if (!subclass) {
+  showError({ statusCode: 404, message: `${useRoute().path} does not exist` });
+}
 </script>
