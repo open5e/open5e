f32ce23034354c5a547fcba2f9dd68870bc5020d
calumbell <calumabell@googlemail.com>
2023-12-20 21:57:41 +0000

    refactored gameplay-mechanics/[section] route w/ useFetchArticle

--- pages/gameplay-mechanics/[section].vue
+++ pages/gameplay-mechanics/[section].vue
@@ -8,27 +8,9 @@
   <p v-else>Loading...</p>
 </template>
 
-<script>
-import axios from 'axios';
-export default {
-  data() {
-    return { section: null };
-  },
-
-  mounted() {
-    const { section } = useRoute().params;
-    const url = `${useRuntimeConfig().public.apiUrl}/sections/${section}/`;
-
-    //you will need to enable CORS to make this work
-    return axios
-      .get(url)
-      .then((response) => (this.section = response.data))
-      .catch(() => {
-        throw showError({
-          statusCode: 404,
-          message: `${useRoute().path} does not exist`,
-        });
-      });
-  },
-};
+<script setup>
+const section = await useFetchArticle({
+  slug: useRoute().params.section,
+  category: 'sections',
+});
 </script>
