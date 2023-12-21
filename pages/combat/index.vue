d637981eda749ec2529c276ac66694e9d64e78f4
calumbell <calumabell@googlemail.com>
2023-12-21 20:19:55 +0000

    fixed linting errors on combat/index page

--- pages/combat/index.vue
+++ pages/combat/index.vue
@@ -2,8 +2,8 @@
   <section class="docs-container container">
     <h1>Combat</h1>
     <div
-      class="flex w-full flex-wrap pt-2 text-lg"
       v-if="combatSections.length == 0"
+      class="flex w-full flex-wrap pt-2 text-lg"
     >
       <div class="flex w-full">
         There are no items for this category that align with the corresponding
@@ -13,7 +13,7 @@
         Please edit your selected sources for more results.
       </div>
     </div>
-    <div class="docs-toc" v-else>
+    <div v-else class="docs-toc">
       <ul v-if="combatSections">
         <li v-for="section in combatSections" :key="section.slug">
           <nuxt-link tag="a" :to="`/combat/${section.slug}`">
