a007a21ddeb254ca675aa4bc2396b6dc5b17da07
sturlen <43607012+Sturlen@users.noreply.github.com>
2024-02-17 13:06:56 +0100

    stub API in e2e test

--- cypress/e2e/monster.cy.js
+++ cypress/e2e/monster.cy.js
@@ -1,6 +1,19 @@
+import dragon from '../fixtures/adult-bronze-dragon.json';
+import search_results from '../fixtures/search.json';
+
 describe('template spec', () => {
   it('can search for a monster', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('http://localhost:3000').wait(1000);
+    cy.intercept(
+      'GET',
+      'https://api.open5e.com/search/?text=Adult%20Bronze%20Dragon',
+      search_results
+    );
+    cy.intercept(
+      'GET',
+      'https://api.open5e.com/monsters/adult-bronze-dragon/',
+      dragon
+    );
     cy.get('input').type('Adult Bronze Dragon{enter}');
     cy.contains('.search-result a', 'Adult Bronze Dragon').click();
     cy.contains('section', 'The dragon can use its Frightful Presence.');
