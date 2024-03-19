9e1f936363c16ad15ae53cdb33df24068abe5a36
calumbell <calumabell@googlemail.com>
2024-03-19 11:55:52 +0000

    refactor: renamed state variables to improve readibility

--- components/SourcesModal.vue
+++ components/SourcesModal.vue
@@ -20,7 +20,7 @@
       <fieldset>
         <div class="space-y-3">
           <div
-            v-for="(group, organization) in groupedDocuments"
+            v-for="(group, organization) in allSourcesByPublisher"
             :key="organization"
           >
             <h3 class="mt-2">{{ organization }}</h3>
@@ -32,7 +32,7 @@
               <div class="flex h-6 items-center">
                 <input
                   :id="document.slug"
-                  v-model="selectedSources"
+                  v-model="sourcesFormState"
                   :name="document.slug"
                   type="checkbox"
                   class="h-4 w-4 rounded border-gray-300 text-blue-600 accent-blood focus:ring-blue-600"
@@ -55,14 +55,14 @@
         ref="cancelButtonRef"
         type="button"
         class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
-        @click="closeModal()"
+        @click="close()"
       >
         Cancel
       </button>
       <button
         type="button"
         class="inline-flex w-full justify-center rounded-md bg-blood px-3 py-2 text-sm font-semibold text-white shadow-sm ring-offset-2 hover:ring-2 hover:ring-blood focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 sm:col-start-2"
-        @click="saveSelection()"
+        @click="save()"
       >
         Update
       </button>
@@ -77,51 +77,60 @@ import SourceTag from '~/components/SourceTag.vue';
 export default {
   data() {
     return {
-      selectedSources: [],
+      sourcesFormState: [], // current state of the sources form data
     };
   },
   computed: {
     store() {
       return useMainStore();
     },
+
+    // array of all currently selected sources
     sourceSelection: function () {
       return this.store.sourceSelection;
     },
-    documents: function () {
+
+    // array of all available sources
+    allSources: function () {
       return this.store.documents;
     },
-    groupedDocuments: function () {
-      return this.documents.reduce((grouped, document) => {
-        (grouped[document.organization] =
-          grouped[document.organization] || []).push(document);
+
+    // object of sources arranged by publisher (key: publisher, value: array of sources)
+    allSourcesByPublisher: function () {
+      return this.allSources.reduce((grouped, source) => {
+        (grouped[source.organization] =
+          grouped[source.organization] || []).push(source);
         return grouped;
       }, {});
     },
   },
+
+  // update form state when the source selection is updated
   watch: {
-    'store.sourceSelection': function (newVal) {
-      this.selectedSources = [...newVal];
+    'store.sourceSelection': function (sourcesFromStore) {
+      this.sourcesFormState = [...sourcesFromStore];
     },
   },
+
   created() {
-    this.selectedSources = this.store.sourceSelection;
+    this.sourcesFormState = this.store.sourceSelection;
   },
   methods: {
-    closeModal() {
+    close() {
       this.$emit('close'); // emits a 'close' event to the parent component
       setTimeout(() => {
-        this.selectedSources = this.store.sourceSelection;
+        this.sourcesFormState = this.store.sourceSelection;
       }, 300);
     },
-    saveSelection() {
-      this.store.setSources(this.selectedSources);
-      this.closeModal();
+    save() {
+      this.store.setSources(this.sourcesFormState);
+      this.close();
     },
     selectAll() {
-      this.selectedSources = this.documents.map((source) => source.slug);
+      this.sourcesFormState = this.allSources.map((source) => source.slug);
     },
     deselectAll() {
-      this.selectedSources = [];
+      this.sourcesFormState = [];
     },
   },
 };
