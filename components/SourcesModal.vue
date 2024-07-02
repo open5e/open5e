54609141f6c4b032bd052f4beaed9627d3fb426b
calum <47755775+calumbell@users.noreply.github.com>
2024-07-02 22:20:09 +0100

    Feature: publisher filters in `<SourcesModal>` (#527)
    * added btn to select all sources by a publisher
    
    * publishers can no be toggled off/on in SourcesModal
    
    * styled publisher selector buttons

--- components/SourcesModal.vue
+++ components/SourcesModal.vue
@@ -1,6 +1,7 @@
 <template>
   <modal-dialog>
     <slot>
+      <!-- Modal menu title bar -->
       <div class="flex w-full justify-between border-b-4 border-red-400">
         <h2 class="mt-0 pb-2">Select Sources</h2>
         <div class="serif font-bold">
@@ -18,43 +19,70 @@
           </button>
         </div>
       </div>
-      <div class="mt-2">
-        <fieldset>
-          <legend class="sr-only">Source Selection</legend>
-          <div
-            v-for="(publications, organization, index) in groupedDocuments"
-            :key="index"
-            class="space-y-1"
-          >
-            <h3 class="mt-2">{{ organization }}</h3>
-            <div
-              v-for="document in publications"
-              :key="document.slug"
-              class="relative flex items-start"
+
+      <fieldset class="mt-1">
+        <legend class="sr-only">Source Selection</legend>
+        <!-- Organisation -->
+        <div
+          v-for="(publications, organization, index) in groupedDocuments"
+          :key="index"
+          class="space-y-0"
+        >
+          <!-- Organisation Title -->
+          <div class="my-1 flex items-center gap-2">
+            <h3 class="mt-0 inline-block items-center gap-2">
+              {{ organization }}
+            </h3>
+            <button
+              :class="
+                selectedSourcesByPublisher(organization)
+                  ? 'border-none bg-blood'
+                  : 'border-2 bg-white'
+              "
+              class="size-5 rounded-sm border-gray-500 text-sm font-bold text-white"
+              @click="togglePublisher(organization)"
             >
-              <div class="flex h-6 items-center">
-                <input
-                  :id="document.slug"
-                  v-model="selectedSources"
-                  :name="document.slug"
-                  type="checkbox"
-                  class="h-4 w-4 rounded border-gray-300 text-blue-600 accent-blood focus:ring-blue-600"
-                  :value="document.slug"
-                />
-              </div>
-              <div class="ml-3 text-sm leading-6">
-                <label
-                  :for="document.slug"
-                  class="font-medium text-gray-900 dark:text-white"
-                >
-                  {{ document.title }}
-                </label>
-                <source-tag :title="document.title" :text="document.slug" />
-              </div>
-            </div>
+              <span
+                v-if="selectedSourcesByPublisher(organization)"
+                class="not-sr-only"
+              >
+                &#10003;
+              </span>
+              <span class="sr-only">
+                {{
+                  selectedSourcesByPublisher(organization)
+                    ? `include sources from ${organization}`
+                    : `exclude sources from ${organization}`
+                }}
+              </span>
+            </button>
           </div>
-        </fieldset>
-      </div>
+
+          <!-- Sources by Organisation -->
+          <ul
+            v-for="document in publications"
+            :key="document.slug"
+            class="relative flex items-start"
+          >
+            <li>
+              <input
+                v-model="selectedSources"
+                :name="document.slug"
+                type="checkbox"
+                class="mr-2 mt-1 h-4 w-4 rounded text-blue-600 accent-blood focus:ring-blue-600"
+                :value="document.slug"
+              />
+              <label
+                :for="document.slug"
+                class="font-medium text-gray-900 dark:text-white"
+              >
+                {{ document.title }}
+              </label>
+              <source-tag :title="document.title" :text="document.slug" />
+            </li>
+          </ul>
+        </div>
+      </fieldset>
     </slot>
     <template #actions>
       <button
@@ -94,12 +122,40 @@ const groupedDocuments = computed(() => {
 });
 
 function closeModal() {
-  console.log(groupedDocuments);
   emit('close'); // emits a 'close' event to the parent component
 }
 function saveSelection() {
   setSources(selectedSources.value);
   closeModal();
+}
+
+function togglePublisher(publisher) {
+  const sourcesByPublisher = groupedDocuments.value[publisher].map(
+    (source) => source.slug // get slugs for sources by publisher
+  );
+
+  if (selectedSourcesByPublisher(publisher)) {
+    selectedSources.value = selectedSources.value.filter(
+      (source) => !sourcesByPublisher.includes(source)
+    );
+  } else {
+    const sourcesToAdd = sourcesByPublisher.filter(
+      (source) => !selectedSources.value.includes(source)
+    );
+    selectedSources.value = [...selectedSources.value, ...sourcesToAdd]; // combine checked & unchecked sources
+  }
+}
+
+function selectedSourcesByPublisher(publisher) {
+  // find all sources for this publisher
+  const allSources = groupedDocuments.value[publisher].map(
+    (source) => source.slug
+  );
+  // find which of these are part of the current selected sources
+  const currentSources = selectedSources.value.filter((source) =>
+    allSources.includes(source)
+  );
+  return currentSources.length;
 }
 
 function selectAll() {
