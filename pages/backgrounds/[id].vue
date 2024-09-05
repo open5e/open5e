75103ad1ac78d80a5d964633316c8d71838ba644
calum <47755775+calumbell@users.noreply.github.com>
2024-09-05 13:36:05 +0200

    API V2 - Backgrounds (#571)
    * switched to backgrounds v2 endpoint
    
    * sorted background features to improve layout

--- pages/backgrounds/[id].vue
+++ pages/backgrounds/[id].vue
@@ -1,40 +1,54 @@
 <template>
   <main v-if="background" class="docs-container container">
     <section>
-      <h1>{{ background.name }}</h1>
-      <source-tag
-        :title="background.document__slug"
-        :text="background.document__slug"
-      />
+      <div>
+        <h1 class="inline">{{ background.name }}</h1>
+        <source-tag
+          class="inline"
+          :title="background.document.name"
+          :text="
+            background.document.url
+              .split('/')
+              .filter((exists) => exists)
+              .pop()
+          "
+        />
+      </div>
       <md-viewer :text="background.desc" />
     </section>
-    <section>
-      <p>
-        <b>Skill Proficiencies: </b>
-        <span> {{ background.skill_proficiencies }}</span>
-      </p>
-      <p v-if="background.tool_proficiencies">
-        <b>Tool Proficiencies: </b>
-        <span> {{ background.tool_proficiencies }}</span>
-      </p>
-      <p v-if="background.languages">
-        <b>Languages: </b>
-        <span> {{ background.languages }}</span>
-      </p>
-      <p>
-        <b>Equipment: </b>
-        <span> {{ background.equipment }}</span>
-      </p>
-    </section>
-    <section>
-      <h2>Feature: {{ background.feature }}</h2>
-      <md-viewer :text="background.feature_desc" />
-    </section>
 
-    <section>
-      <h2>Suggested Characteristics</h2>
-      <md-viewer :text="background.suggested_characteristics" />
-    </section>
+    <!-- List of proficiencies & other short, mechanical benefits -->
+    <dl class="mt-2">
+      <div v-for="benefit in benefits.proficiencies" :key="benefit.name">
+        <dt class="inline font-bold">{{ benefit.name }}</dt>
+        <dd class="inline before:content-['._']">{{ benefit.desc }}</dd>
+      </div>
+    </dl>
+
+    <!-- List of background features -->
+    <ul>
+      <li v-for="benefit in benefits.features" :key="benefit.name">
+        <h2>{{ `Feature: ${benefit.name}` }}</h2>
+        <md-viewer :text="benefit.desc" />
+      </li>
+    </ul>
+    <hr />
+
+    <!-- List of background flavour, rollable tables, etc. -->
+    <ul>
+      <li v-for="benefit in benefits.flavour" :key="benefit.name">
+        <h3>
+          <span
+            v-if="benefit.type === 'feature'"
+            class="mr-2 capitalize after:content-[':_']"
+          >
+            {{ benefit.type }}
+          </span>
+          <span>{{ benefit.name }}</span>
+        </h3>
+        <md-viewer :text="benefit.desc" />
+      </li>
+    </ul>
   </main>
   <p v-else>Loading...</p>
 </template>
@@ -44,4 +58,35 @@ const { data: background } = useFindOne(
   API_ENDPOINTS.backgrounds,
   useRoute().params.id
 );
+
+// sort benefits into different sections
+// different sections will be rendered to different parts of the page
+const benefits = computed(() => {
+  const [proficiencies, features, flavour] = background.value.benefits.reduce(
+    (acc, benefit) => {
+      // sort profs, langs, equipment, &c into 'proficiencies'
+      if (
+        [
+          'equipment',
+          'language',
+          'skill_proficiency',
+          'tool_proficiency',
+          'ability_score',
+        ].includes(benefit.type)
+      ) {
+        return [[...acc[0], benefit], acc[1], acc[2]];
+      }
+
+      // sort features into 'features'
+      if (benefit.type === 'feature') {
+        return [acc[0], [...acc[1], benefit], acc[2]];
+      }
+
+      // base-case: sort remaining benefits into 'flavour'
+      return [acc[0], acc[1], [...acc[2], benefit]];
+    },
+    [[], [], []]
+  );
+  return { proficiencies, features, flavour };
+});
 </script>
