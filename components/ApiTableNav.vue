90353dab35fc12a665fde499270901b0a8a90784
calumbell <calumabell@googlemail.com>
2024-09-10 22:30:32 +0100

    fixed bugs with ApiTableNav

--- components/ApiTableNav.vue
+++ components/ApiTableNav.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="grid w-full justify-end">
     <ul class="flex">
-      <li
-        v-for="button in buttons"
-        :key="button.name"
-        :aria-roledescription="button.name"
-      >
+      <li v-for="button in buttons" :key="button.name">
         <button
           class="mt-1 rounded-md border-2 px-2 py-1 text-fog"
+          :disabled="!button.isActive.value"
           :class="
             button.isActive.value
               ? 'bg-blood hover:bg-fog hover:text-blood'
               : 'bg-slate-800'
           "
+          :aria-roledescription="button.name"
           @click="button.onClick()"
         >
           <span class="sr-only">{{ button.name }}</span>
@@ -32,6 +30,7 @@ const props = defineProps({
 });
 const emit = defineEmits(['first', 'last', 'next', 'prev']);
 
+// conditional properties control whenever certain buttons are enabled
 const isNotFirstPage = computed(() => props.pageNumber > 1);
 const isNotLastPage = computed(() => props.pageNumber < props.lastPageNumber);
 
