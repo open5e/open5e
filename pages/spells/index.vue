23ffa2ee6a7c55c0ee1058b25aefe8a717b693d8
calumbell <calumabell@googlemail.com>
2024-09-22 18:23:44 +0100

    /spells table now filterable

--- pages/spells/index.vue
+++ pages/spells/index.vue
@@ -1,18 +1,97 @@
 <template>
   <section class="docs-container container">
-    <div class="filter-header-wrapper">
-      <h1 class="filter-header">Spells</h1>
+    <div class="flex">
+      <h1 class="my-2">Spells</h1>
+      <api-table-nav
+        :page-number="pageNo"
+        :last-page-number="lastPageNo"
+        @first="firstPage()"
+        @next="nextPage()"
+        @prev="prevPage()"
+        @last="lastPage()"
+      />
     </div>
 
-    <api-table-nav
-      :page-number="pageNo"
-      :last-page-number="lastPageNo"
-      @first="firstPage()"
-      @next="nextPage()"
-      @prev="prevPage()"
-      @last="lastPage()"
-    />
+    <!-- SEARCH FILTERS -->
+    <div class="flex gap-3 py-1">
+      <div class="relative">
+        <icon
+          name="majesticons:search-line"
+          class="absolute bottom-1.5 mr-2 h-4 w-4"
+        />
+        <input
+          id="spellName"
+          default=""
+          name="spellName"
+          placeholder="Search by spell name..."
+          class="border-b-2 border-red-400 bg-transparent pl-6"
+          @input="update('name__contains', $event.target.value)"
+        />
+      </div>
 
+      <select
+        id="spellLevel"
+        name="spellLevel"
+        class="bg-transparent text-center"
+        value=""
+        @input="update('level', $event.target.value)"
+      >
+        <option value="" default>–</option>
+        <option
+          v-for="level in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]"
+          :key="level"
+          :value="level"
+        >
+          {{ SPELL_LEVELS_NAMES[level] }}
+        </option>
+      </select>
+      <select
+        id="spellSchool"
+        name="spellSchool"
+        class="bg-transparent"
+        @input="update('school__key', $event.target.value)"
+      >
+        <option value="" class="text-smoke">–</option>
+        <option
+          v-for="school in [
+            'Abjuration',
+            'Conjuration',
+            'Divination',
+            'Enchantment',
+            'Evocation',
+            'Illusion',
+            'Necromancy',
+            'Transmutation',
+          ]"
+          :key="school"
+          :value="school.toLowerCase()"
+        >
+          {{ school }}
+        </option>
+      </select>
+      <select
+        id="spellClassList"
+        name="spellClassList"
+        class="bg-transparent"
+        @input="update('classes__key__in', $event.target.value)"
+      >
+        <option value="" class="text-smoke">-</option>
+        <option
+          v-for="charClass in [
+            'Bard',
+            'Cleric',
+            'Druid',
+            'Sorcerer',
+            'Warlock',
+            'Wizard',
+          ]"
+          :key="charClass"
+          :value="'srd_' + charClass.toLowerCase()"
+        >
+          {{ charClass }}
+        </option>
+      </select>
+    </div>
     <api-results-table
       :data="data?.results"
       :cols="[
@@ -56,13 +135,16 @@
 const { sortBy, isSortDescending, setSortState } = useSortState();
 
 // fields to fetch from API to populate table
-const fields = ['name', 'document', 'level', 'school', 'verbal', 'classes'];
+const fields = ['name', 'document', 'level', 'school', 'classes'];
+
+const { filter, update } = useFilterState(DefaultMonsterFilter);
 
 // Fetch a page of results and pagination controls
 const { data, paginator } = useFindPaginated({
   endpoint: API_ENDPOINTS.spells,
   sortByProperty: sortBy,
   isSortDescending: isSortDescending,
+  filter,
   params: { fields, depth: 1 },
 });
 
