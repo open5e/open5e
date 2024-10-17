c4c95072ea21f6fc79354706440a7225ec7a9d40
calumbell <calumabell@googlemail.com>
2024-10-17 13:15:07 +0100

    added weapon detail view to /equipment/[id]

--- pages/equipment/[id].vue
+++ pages/equipment/[id].vue
@@ -1,22 +1,64 @@
 <template>
   <section class="docs-container container">
     <div v-if="item">
-      <h1 class="inline">{{ item.name }}</h1>
+      <h1>{{ item.name }}</h1>
+
+      <!-- ITEM CARD FOR WEAPONS -->
+      <div v-if="item.weapon">
+        <p class="italic">{{ formatWeaponSubtitle(item.weapon) }}</p>
+        <dl class="grid auto-cols-max grid-cols-2">
+          <dt class="font-bold">Damage</dt>
+          <dd>
+            {{
+              item.weapon.damage_dice +
+              (item.weapon.is_versatile
+                ? ` (${item.weapon.versatile_dice})`
+                : '')
+            }}
+          </dd>
+          <dt class="font-bold">Damage Type</dt>
+          <dd class="capitalize">
+            {{ item.weapon.damage_type.split('/').slice(-2)[0] }}
+          </dd>
+          <dt class="font-bold">Properties</dt>
+          <dd class="capitalize">
+            {{ item.weapon.properties.map((prop) => prop).join(', ') }}
+          </dd>
+          <template v-if="item.weapon.is_reach">
+            <dt class="font-bold">Reach</dt>
+            <dd>{{ item.weapon.reach + ' ft.' }}</dd>
+          </template>
+          <template v-if="item.weapon.range">
+            <dt class="font-bold">Range</dt>
+            <dd>{{ `${item.weapon.range} / ${item.weapon.long_range}` }}</dd>
+          </template>
+          <template v-if="item.weight">
+            <dt class="font-bold">Weight</dt>
+            <dd>{{ formatWeight(item.weight) }}</dd>
+          </template>
+          <template v-if="item.cost">
+            <dt class="font-bold">Cost</dt>
+            <dd>{{ formatCost(item.cost) }}</dd>
+          </template>
+        </dl>
+      </div>
 
       <!-- DISPLAY COMMON ITEM DATA: category, cost, weight, &c -->
-      <dl class="flex gap-2">
-        <dt class="font-bold">Category</dt>
-        <dd>{{ item.category.name }}</dd>
-        <template v-if="parseFloat(item.weight) > 0">
-          <dt class="font-bold before:content-['_|_']">Weight</dt>
-          <dd>{{ formatWeight(item.weight) }}</dd>
-        </template>
-        <template v-if="parseFloat(item.cost) > 0">
-          <dt class="font-bold before:content-['_|_']">Cost</dt>
-          <dd>{{ formatCost(item.cost) }}</dd>
-        </template>
-      </dl>
-
+      <div v-else>
+        <dl class="flex gap-2">
+          <dt class="font-bold">Category</dt>
+          <dd>{{ item.category.name }}</dd>
+          <template v-if="parseFloat(item.weight) > 0">
+            <dt class="font-bold before:content-['_|_']">Weight</dt>
+            <dd>{{ formatWeight(item.weight) }}</dd>
+          </template>
+          <template v-if="parseFloat(item.cost) > 0">
+            <dt class="font-bold before:content-['_|_']">Cost</dt>
+            <dd>{{ formatCost(item.cost) }}</dd>
+          </template>
+        </dl>
+        <md-viewer :text="item.desc" />
+      </div>
       <p class="text-sm italic">
         Source:
         <a target="NONE" :href="item.document.permalink">
@@ -39,7 +81,6 @@ const { data: item } = useFindOne(
 const formatCost = (input) => {
   const [gold, rest] = input.split('.');
   const [silver, copper] = rest.split('');
-  console.log(gold, silver, copper);
   return (
     (parseInt(gold) > 0 ? `${gold} gp` : '') +
     (parseInt(silver) > 0 ? `${silver} sp` : '') +
@@ -52,4 +93,9 @@ const formatWeight = (input) => {
   if (weight >= 1) return `${weight} lb`;
   return `1 lb for ${1 / weight}`;
 };
+
+const formatWeaponSubtitle = (weapon) =>
+  `${weapon.is_melee ? 'Melee' : 'Ranged'} weapon ` +
+  `(${weapon.is_martial ? 'martial' : 'simple'}, ` +
+  `${weapon.name.toLowerCase()})`;
 </script>
