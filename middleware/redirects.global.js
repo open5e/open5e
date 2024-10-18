6894e876f869501795e769c31027eb3e20b7c72c
calumbell <calumabell@googlemail.com>
2024-10-18 19:43:53 +0100

    updated redirect middleware to better handle likely v1/v2 collisions

--- middleware/redirects.global.js
+++ middleware/redirects.global.js
@@ -1,64 +1,47 @@
-// add redirects for exact path matches here
-const redirects = {
+// if a url matches a key, trigger redirect to value
+const exactMatchesToRedirect = {
   '/monsters/monster-list': '/monsters',
   '/magicitems/magicitem-list': '/magic-items',
   '/spells/spells-table': '/spells',
   '/magicitems': '/magic-items',
+  '/sections': '/rules',
 };
 
-// add redirects for partial path matches here
-const substitutions = [
-  {
-    find: '/magicitems',
-    replaceWith: '/magic-items',
-  },
+// if a url contains 'find' as a substring, trigger redirect to 'to'
+const partialMatchesToRedirect = [
+  { find: '/combat', to: '/rules' },
+  { find: '/characters', to: '/rules' },
+  { find: '/gameplay-mechanics', to: '/rules' },
+  { find: '/running', to: '/rules' },
 ];
 
-// redirects from /sections routes require extra data from API
-async function replaceSectionsWithParent(path) {
-  const slug = path.split('/')[2]; // isolate UID
-  const apiURL = useRuntimeConfig().public.apiUrl;
-  const endpoint = `${apiURL}/sections/${slug}`;
-
-  // fetch section parent & create redirect URL
-  const { data } = await useFetch(endpoint);
-  if (!data?.value) {
-    return;
-  }
-
-  // slugify section parent
-  const parent = data.value.parent
-    .toLowerCase()
-    .replace(/\s+/g, '-') // replace spaces with hyphens
-    .replace('rules', 'gameplay-mechanics'); // handle common inconsistancy in data-set
-  return `/${parent}/${slug}`;
-}
+// if a url contains a substring, replace it with another substring
+const substitutions = [{ find: '/magicitems', replaceWith: '/magic-items' }];
 
 export default defineNuxtRouteMiddleware((to) => {
-  // redirecting only works client-side, return if server-side
-  if (process.server) {
-    return;
-  }
-
   // remove terminal slash from path
   const path = to.fullPath.endsWith('/')
     ? to.fullPath.slice(0, -1)
     : to.fullPath;
 
-  // if entire path is present in redirects obj, navigate to it
-  if (redirects[path]) {
-    return navigateTo(redirects[path], { redirectCode: 301 });
+  // handle exact url match redirects
+  if (exactMatchesToRedirect[path]) {
+    return navigateTo(exactMatchesToRedirect[path], { redirectCode: 301 });
   }
 
-  // check whether any part of the path needs to be replaced
+  // handle partial url match redirects
+  const partialMatchRedirect = partialMatchesToRedirect
+    .map(({ find, to }) => {
+      if (path.search(find) > -1) return to;
+    })
+    .filter((exists) => !!exists);
+  if (partialMatchRedirect.length > 0)
+    return navigateTo(partialMatchRedirect[0], { redirectCode: 301 });
+
+  // finally, check if any substring substitutions need to be made
   substitutions.forEach(({ find, replaceWith }) => {
     if (path.search(find) > -1) {
       return navigateTo(path.replace(find, replaceWith), { redirectCode: 301 });
     }
   });
-
-  // check whether a /section/ route needs to be replaced with parent from API
-  if (path.search('/sections/') > -1) {
-    return navigateTo(replaceSectionsWithParent(path), { redirectCode: 301 });
-  }
 });
