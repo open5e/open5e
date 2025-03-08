fc5f09edaf7a79a9742c9bf173ccf047ee9ddc66
Jesse Carrigan <jesse.m.carrigan@gmail.com>
2025-03-08 06:03:29 -0800

    Added accidentally removed traits section

--- pages/monsters/[id].vue
+++ pages/monsters/[id].vue
@@ -227,6 +227,19 @@
       </ul>
     </section>
 
+    <!-- TRAITS -->
+    <section v-if="monster.traits?.length !== 0">
+      <h2>Traits</h2>
+      <ul id="traits-list">
+        <li v-for="trait in monster.traits" :key="trait.key" class="my-1">
+          <span class="font-bold after:content-['._']">
+            {{ trait.name }}
+          </span>
+          <md-viewer :inline="true" :text="trait.desc" :use-roller="true" />
+        </li>
+      </ul>
+    </section>
+
     <!-- ACTIONS -->
     <section v-if="monster.actions.length !== 0">
       <h2>Actions</h2>
