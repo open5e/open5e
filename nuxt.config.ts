f08d7a7a86c67d9cddbf107395bb900324849735
eepMoody <moody.designer@gmail.com>
2025-03-08 01:03:32 -0500

    cleanup from local development

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -72,7 +72,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiUrl: process.env.API_URL || 'http://127.0.0.1:8000/',
+      apiUrl: process.env.API_URL || 'https://api.open5e.com',
     },
   },
 
