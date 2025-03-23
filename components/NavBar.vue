4acc867a9b7b30729e80a9e9c46fb2f0b1377e90
calumbell <calumabell@googlemail.com>
2025-03-23 16:37:34 +0000

    base- & sub-class links show on navbar on /classes & child routes

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -14,19 +14,35 @@
 
 <template>
   <ul class="text-inherit text-white">
+    <TestComponent />
+    <!-- Render top-level links -->
     <li v-for="section in routes" :key="section.title">
-      <nav-link :to="section.route">
+      <nav-link :to="section.url">
         {{ section.title }}
       </nav-link>
+
+      <!-- Render sub-routes -->
       <ul
         v-if="section.subroutes"
-        v-show="path.indexOf(section.route) != -1"
+        v-show="crumbs?.[0]?.title === section.title"
         class="bg-slate-800/30 py-2"
       >
-        <li v-for="page in section.subroutes" :key="page.key">
-          <nav-link :to="`${section.route}/${page.key}`" :indent="true">
-            {{ page.name }}
+        <li v-for="subroute in section.subroutes" :key="subroute.title">
+          <nav-link :to="`${subroute.url}`" :indentation-level="1">
+            {{ subroute.title }}
           </nav-link>
+
+          <!-- Render sub-sub routes (and no deeper) -->
+          <ul>
+            <li
+              v-for="subSubroute in subroute.subroutes"
+              :key="subSubroute.title"
+            >
+              <nav-link :to="`${subSubroute.url}`" :indentation-level="2">
+                {{ subSubroute.title }}
+              </nav-link>
+            </li>
+          </ul>
         </li>
       </ul>
     </li>
@@ -34,59 +50,93 @@
 </template>
 
 <script setup>
-import { useRoute } from 'nuxt/app';
-const { path } = useRoute();
-
 const { data: classes } = useFindMany(API_ENDPOINTS.classes, {
-  fields: ['name', 'key'].join(),
-  is_subclass: false,
+  fields: ['name', 'key', 'subclass_of'].join(),
+});
+
+const crumbs = useBreadcrumbs();
+
+// Conditionally generates additional links to base- and sub- classes
+const classSubroutes = computed(() => {
+  if (!classes?.value) return;
+  // only generate subroutes if we are on a child route of '/classes'
+  if (crumbs.value?.[0]?.title !== 'Classes') return;
+
+  // split classes into base- and sub-classes
+  const [baseClasses, subClasses] = classes.value.reduce(
+    (acc, val) => {
+      if (!val['subclass_of']) return [[...acc[0], val], [...acc[1]]];
+      else return [[...acc[0]], [...acc[1], val]];
+    },
+    [[], []]
+  );
+
+  // generate subroutes to other base-classes
+  let output = baseClasses.map((item) => {
+    return { title: item.name, url: `/classes/${item.key}` };
+  });
+
+  // if we are on a sub-route of a base-class, get other sub-class routes
+  if (crumbs.value.length >= 2) {
+    const baseClass = crumbs.value[1];
+    const subClassesForClass = subClasses
+      .filter((item) => item?.['subclass_of']?.includes(baseClass.src))
+      .map((item) => {
+        const url = `/classes/${baseClass.src}/${item.key}`;
+        return { title: item.name, url };
+      });
+    output[output.findIndex((el) => el.title === baseClass.title)].subroutes =
+      subClassesForClass;
+  }
+
+  return output;
 });
 
 const routes = computed(() => [
   {
     title: 'Classes',
-    route: '/classes',
-    subroutes: classes.value ?? [],
+    url: '/classes',
+    subroutes: classSubroutes.value,
   },
   {
     title: 'Races',
-    route: '/races',
+    url: '/races',
   },
   {
     title: 'Monsters',
-    route: '/monsters',
+    url: '/monsters',
   },
   {
     title: 'Magic Items',
-    route: '/magic-items',
+    url: '/magic-items',
   },
   {
     title: 'Spells',
-    route: '/spells',
+    url: '/spells',
   },
   {
     title: 'Backgrounds',
-    route: '/backgrounds',
+    url: '/backgrounds',
   },
   {
     title: 'Feats',
-    route: '/feats',
+    url: '/feats',
   },
   {
     title: 'Equipment',
-    route: '/equipment',
+    url: '/equipment',
   },
   {
     title: 'Conditions',
-    route: '/conditions',
+    url: '/conditions',
   },
   {
     title: 'Rules',
-    route: '/rules',
+    url: '/rules',
   },
   {
     title: 'API Docs',
-    route: '/api-docs',
+    url: '/api-docs',
   },
 ]);
 </script>
