3edf15fd4c6d4454a28d8b80601f9f01a18b8465
calumbell <calumabell@googlemail.com>
2025-06-16 23:01:09 +0100

    notifications are now rmv'd when clicked

--- components/PageNotifications.vue
+++ components/PageNotifications.vue
@@ -28,7 +28,8 @@
       <li
         v-for="(notification, index) in notifications"
         :key="index"
-        class="mt-1 border-y-2 border-red-400 bg-gray-100 px-3 py-2 dark:bg-slate-900"
+        class="my-2 cursor-pointer border-y-2 border-fireball bg-gray-100 px-3 py-2 transition-all duration-150 hover:border-mana dark:bg-slate-900"
+        @click="remove(index)"
       >
         <p class="my-0 font-serif text-xs font-light">
           {{ notification.title }}
@@ -37,12 +38,6 @@
           <p class="m-0 text-4xl">
             {{ notification.body }}
           </p>
-          <button
-            class="float-right font-bold text-blood transition-all hover:text-red-400"
-            @click="remove(index)"
-          >
-            &#x2715;
-          </button>
         </div>
         <div class="m-0 p-0 text-sm">
           {{ notification.footer }}
