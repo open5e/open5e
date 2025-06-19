e342e4281b3d0e31b253a5d180229e480a0c1569
calumbell <calumabell@googlemail.com>
2025-06-19 20:16:27 +0100

    decreased size of older notifications

--- components/PageNotifications.vue
+++ components/PageNotifications.vue
@@ -27,31 +27,43 @@
     </div>
 
     <!-- Notification list -->
-    <ul class="flex flex-col text-center ">
+    <ul class="flex w-min flex-col text-nowrap text-center">
       <li
         v-for="(notification, index) in notifications"
         :key="index"
-        class="my-2 cursor-pointer border-y-2 border-fireball bg-gray-100 px-3 py-2 duration-150 hover:border-mana dark:bg-slate-900"
+        class="my-2 cursor-pointer border-y-2 border-blood bg-gray-100 px-3 py-2 duration-150 hover:border-fireball dark:bg-slate-900"
         @click="remove(index)"
       >
-        <div class="text-sm">
-          <span class="my-0 py-0 font-serif font-light capitalize">
-            {{ notification.title }}
-          </span>
-          <span
-            v-if="notification.subtitle"
-            class="uppercase text-basalt before:content-['_|_'] dark:text-smoke"
-          >
-            {{ notification.subtitle }}
-          </span>
+        <!-- SMALL VARIATION: use for all notifications except the latest -->
+        <div 
+          v-if="index < notifications.length - 1"
+          class="text-nowrap text-right text-xs capitalize"
+        >
+          <span class="font-serif">{{ notification.title }}</span>
+          <span v-if="notification.subtitle" class="before:content-['_|_']">{{ notification.subtitle }}</span>
+          <span class="mx-2 text-base font-bold before:content-['_']">{{ notification.body }}</span>
         </div>
-        <div class="my-0 flex justify-center align-middle font-bold">
-          <p class="m-0 text-5xl">
-            {{ notification.body }}
-          </p>
-        </div>
-        <div class="m-0 p-0 text-sm text-basalt dark:text-smoke">
-          {{ notification.footer }}
+        <!-- EXPANDED VARIATION: use for the most recent notification -->
+        <div v-else>
+          <div class="text-sm">
+            <span class="my-0 py-0 font-serif font-light capitalize">
+              {{ notification.title }}
+            </span>
+            <span
+              v-if="notification.subtitle"
+              class="uppercase text-basalt before:content-['_|_'] dark:text-smoke"
+            >
+              {{ notification.subtitle }}
+            </span>
+          </div>
+          <div class="my-0 flex justify-center align-middle font-bold">
+            <p class="m-0 text-5xl">
+              {{ notification.body }}
+            </p>
+          </div>
+          <div class="m-0 p-0 text-sm text-basalt dark:text-smoke">
+            {{ notification.footer }}
+          </div>
         </div>
       </li>
     </ul>
