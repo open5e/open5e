4af4416fee76d87c9a9ded34d7c6a58d6c6269cb
calumbell <calumabell@googlemail.com>
2025-07-13 20:39:56 +0100

    exported basic data types from /type dir

---
+++ types/index.ts
@@ -0,0 +1,18 @@
+import type { components } from './open5e-api';
+
+// export basic data types returned by Open5e API
+export type Background = components['schemas']['Background'];
+export type Class = components['schemas']['CharacterClass'];
+export type ClassSummary = components['schemas']['CharacterClassSummary'];
+export type Condition = components['schemas']['Condition'];
+export type Feat = components['schemas']['Feat'];
+export type Rule = components['schemas']['Rule'];
+export type RuleSet = components['schemas']['RuleSet'];
+export type SearchResult = components['schemas']['SearchResult'];
+export type Species = components['schemas']['Species'];
+export type Spell = components['schemas']['Spell'];
+export type MagicItem = components['schemas']['Item'];
+export type Monster = components['schemas']['Creature'];
+export type Item = components['schemas']['Item'];
+export type ItemCategory = components ['schemas']['ItemCategory'];
+export type ItemCategorySummary = components['schemas']['ItemCategorySummary'];
