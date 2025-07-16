ff61c15a430f060d69375be19be55ae0a472ecfe
calumbell <calumabell@googlemail.com>
2025-07-16 22:39:01 +0100

    added social media links to footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,14 +1,39 @@
 <template>
   <footer class="mt-2 w-full border-t-2 border-fireball bg-white py-2 text-center align-middle dark:bg-darkness dark:text-white ">
-    <NuxtLink to="/legal" class="font-bold hover:underline">Legal</NuxtLink>
+    <ul class="flex justify-center gap-4">
+      <li v-for="{ name, url } in socialMediaLinks" :key="name">
+        <a :href="url" :title="name">
+          {{ name }}
+          <!-- <span class="sr-only">{{ name }}</span> -->
+        </a>
+      </li>
+    </ul>
+    <NuxtLink
+      to="/legal"
+      class="font-bold hover:underline"
+      title="Legal"
+    >
+      Legal
+    </NuxtLink>
     <p class="text-charcoal dark:text-smoke">Open5e</p>
+   
   </footer>
 </template>
 
-<script lang="ts" setup>
+<script setup>
 
+const socialMediaLinks = [
+  {
+    name: 'Discord',
+    url: 'https://discord.com/invite/QXqF6gSVqB',
+  },
+  {
+    name: 'Github',
+    url: 'https://www.github.com/open5e',
+  },
+  {
+    name: 'Patreon',
+    url: 'https://www.patreon.com/open5e',
+  }
+];
 </script>
-
-<style>
-
-</style>
