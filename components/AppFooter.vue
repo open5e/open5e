2e92fc014c3d05a2fd56c0ad6e17949c5017db8a
calumbell <calumabell@googlemail.com>
2025-07-16 21:41:46 +0100

    created new AppFooter components, added to layout

---
+++ components/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="mt-2 w-full border-t-2 border-fireball bg-white py-2 text-center align-middle dark:bg-darkness dark:text-white ">
+    <NuxtLink to="/legal" class="font-bold hover:underline">Legal</NuxtLink>
+    <p class="text-charcoal dark:text-smoke">Open5e</p>
+  </footer>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -131,6 +131,7 @@
         </div>
       </div>
     </div>
+    <AppFooter />
   </div>
 </template>
 
