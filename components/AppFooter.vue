c47dd41621eada1866564aba2736a67e95237a48
calumbell <calumabell@googlemail.com>
2025-07-17 21:16:45 +0100

    added brand icons to social media links

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,10 +1,14 @@
 <template>
-  <footer class="mt-2 w-full border-t-2 border-fireball bg-white py-2 text-center align-middle dark:bg-darkness dark:text-white ">
-    <ul class="flex justify-center gap-4">
-      <li v-for="{ name, url } in socialMediaLinks" :key="name">
+  <footer class="mt-2 grid w-full justify-center border-t-2 border-fireball bg-white py-2 text-center align-middle dark:bg-darkness dark:text-white ">
+    <ul class="mb-2 flex justify-center gap-8">
+      <li v-for="{ name, url, icon } in socialMediaLinks" :key="name">
         <a :href="url" :title="name">
-          {{ name }}
-          <!-- <span class="sr-only">{{ name }}</span> -->
+          <img 
+            v-if="icon"
+            :src="icon"
+            class="size-8 rounded-full border-granite bg-black p-[.35rem] transition-colors hover:bg-granite dark:border dark:bg-charcoal dark:hover:bg-granite"
+          />
+          <span class="sr-only">{{ name }}</span>
         </a>
       </li>
     </ul>
@@ -26,14 +30,17 @@ const socialMediaLinks = [
   {
     name: 'Discord',
     url: 'https://discord.com/invite/QXqF6gSVqB',
+    icon: '/icons/discord-mark.svg',
   },
   {
     name: 'Github',
     url: 'https://www.github.com/open5e',
+    icon: '/icons/github-mark.svg'
   },
   {
     name: 'Patreon',
     url: 'https://www.patreon.com/open5e',
+    icon: '/icons/patreon-mark.svg'
   }
 ];
 </script>
