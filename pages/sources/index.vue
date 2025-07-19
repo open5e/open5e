390518f1a15bbd92a10cadf5ec3cb1ae206688e8
calumbell <calumabell@googlemail.com>
2025-07-19 12:36:09 +0100

    /sources page now displays list of source docs arranged by publisher

--- pages/sources/index.vue
+++ pages/sources/index.vue
@@ -2,15 +2,45 @@
   <section class="docs-container container">
     <div>
       <h1>Sources</h1>
+
+      <!-- Outer list is a list of Publishers -->
       <ul>
-        <li v-for="document in documents" :key="document.name">
-          {{ document.name }}
+        <li 
+          v-for="(docs, publisher) in sortDocumentsByPublisher(documents)"
+          :key="publisher"
+        >
+          <!-- Inner list is a list of Sources per Publisher -->
+          <h2 class="w-min text-nowrap">{{ publisher }}</h2>
+          <dl class="flex flex-col">
+
+            <!-- Card for each Source -->
+            <div 
+              v-for="doc in docs"
+              :key="doc.name"
+              class="my-2 border bg-fog p-2 dark:bg-basalt"
+            >
+              <dt class="inline text-lg font-bold after:content-['_']">
+                <span>{{ doc.name }}</span>
+                <SourceTag :text="doc.key" />
+              </dt>
+              <dd>
+                <p class="mt-0">{{ doc.desc.split(" ").slice(0,50).join(" ") }}</p>
+                <p v-if="doc.licenses.length > 0" class="mt-0 text-sm text-gray-500">
+                  {{`
+                    Published under the 
+                    ${doc.licenses.map((l) => l.key).join(' / ').toUpperCase()}
+                    ${" " + doc.licenses.length > 1 ? "licenses" : "license"}`}}
+                </p>
+              </dd>
+            </div>
+          </dl> <!-- End of inner list -->
         </li>
-      </ul>
+      </ul> <!-- End of outer list -->
     </div>
   </section>
 </template>
 
 <script setup>
+import { sortDocumentsByPublisher } from '../../functions/sortDocumentsByPublisher';
 const { data: documents } = useDocuments();
 </script>
