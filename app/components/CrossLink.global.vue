403ffac8b13e076c0ba09acdd350b3f7b3faa58e
calumbell <calumabell@googlemail.com>
2025-11-19 10:36:12 +0000

    updated LinkPreview component to work with v2 API

--- app/components/CrossLink.global.vue
+++ app/components/CrossLink.global.vue
@@ -3,9 +3,11 @@
   <!-- If preview data has fetched correctly then url is valid, render as link -->
   <nuxt-link
     v-if="previewData"
+    class="group relative"
     :to="`${topLevelPage}/${key}`"
   >
     <slot />
+    <LinkPreview :data="previewData" :category="topLevelPage" />
   </nuxt-link>
 
   <span v-else class="italic">
@@ -14,7 +16,7 @@
 </template>
 
 <script setup lang="ts">
-import type { Item } from '@/types';
+import type { Class, Item } from '@/types';
 
 const props = defineProps({ src: { type: String, default: '' } });
 
@@ -24,25 +26,25 @@ type CrossLinkEndpoint = 'v2/items/' | 'v2/creatures/' | 'v2/classes/' | 'v2/spe
 
 const versionWithEndpoint = `${version}/${endpoint}/` as CrossLinkEndpoint;
 
-const baseFields = ['name', 'key'];
+const baseFields = ['name', 'key', 'document'];
 const queryParametersPerEndpoint = {
-  'v2/items/': [...baseFields, 'rarity', 'type'],
-  'v2/creatures/': [...baseFields, 'type', 'size'],
-  'v2/spells/': [...baseFields, 'level', 'school']
+  'v2/items/': [...baseFields, 'rarity', 'category'],
+  'v2/creatures/': [...baseFields, 'type', 'size', 'challenge_rating_text'],
+  'v2/spells/': [...baseFields, 'level', 'school'],
+  'v2/classes/': [...baseFields, 'subclass_of'],
 } as Record<CrossLinkEndpoint, string[]>;
 
-// fetch data to use in mouse-over preview
-const previewData = computed(() => {
-  if (!version || !endpoint || !key) return;
-  const { data } = useFindOne(versionWithEndpoint, key, {
+const { data } = version && endpoint && key 
+  ? useFindOne(versionWithEndpoint, key, {
     params: {
       fields: (
         queryParametersPerEndpoint[versionWithEndpoint] ?? baseFields
       ).join(',')
     }
-  });
-  return data.value;
-});
+  })
+  : { data: ref(null) };
+
+const previewData = computed(() => data.value);
 
 const topLevelPage = computed(() => {
   // format top-level page part of URL where it differs from API structure
@@ -52,10 +54,15 @@ const topLevelPage = computed(() => {
       ? '/magic-items'
       : '/equipment';
   }
+  if (versionWithEndpoint === 'v2/classes/') {
+    const subclassOf = (previewData.value as Class).subclass_of;
+    if (!subclassOf) return '/classes';
+    return '/classes/' + subclassOf.key;
+  }
+    
   if (versionWithEndpoint === 'v2/creatures/') {
     return '/monsters';
   }
-
   return '/' + endpoint; // Base case
 });
 </script>
--- app/components/LinkPreview.vue
+++ app/components/LinkPreview.vue
@@ -1,102 +1,65 @@
 <template>
-  <article
-    class="absolute top--1 z-10 hidden h-max bg-slate-100 px-4 py-3 text-black shadow-md dark:bg-basalt dark:text-white md:group-hover:block"
-  >
-    <!-- Generate card title from data -->
-    <p class="m-0 whitespace-nowrap border-b-2 border-blood pb-1 align-middle">
-      <span class="font-serif text-lg font-bold text-blood dark:text-white">
-        {{ content.name }}
-      </span>
-      <span class="before:mx-2 before:font-bold before:content-['|']">
-        {{ subtitle }}
-      </span>
+  <article class="group absolute top--1 z-10 hidden border border-red bg-fog p-2 text-charcoal group-hover:block dark:bg-charcoal dark:text-fog">
+    <p class="mt-0 text-nowrap">
+      <span class=" font-serif text-lg text-black dark:text-white">{{ data.name }}</span>
+      <span v-if="categoryDisplayName" class="mx-1 font-bold text-red">{{ " | " }}</span>
+      <span v-if="categoryDisplayName" class="text-lg">{{ categoryDisplayName }}</span>
     </p>
-
-    <!-- Generate card body from data -->
-    <p
-      v-for="item in body"
-      :key="item.title"
-      class="m-0 p-0 text-sm"
-    >
-      <span
-        v-if="item.title"
-        class="font-bold after:mr-1 after:content-[':']"
-      >
-        {{ item.title }}
-      </span>
-      <span>{{ item.data }} </span>
+    <p v-if="subtitle" class="mt-0 text-nowrap text-base">
+      {{ subtitle }}
     </p>
-
-    <!-- Card footer -->
-    <p
-      v-if="content.document__title"
-      class="whitespace-nowrap text-sm italic"
-    >
-      Source: {{ content.document__title }}
+    <p class="mt-0 text-nowrap text-sm italic text-granite dark:text-smoke">
+      {{ `${category}/${data.key}`.slice(1) }}
     </p>
   </article>
 </template>
 
-<script setup>
-const props = defineProps({
-  category: { type: String, default: '' },
-  // eslint-disable-next-line vue/require-prop-types
-  content: { default: [] },
+<script setup lang="ts">
+import type { Creature, Class, Item, Open5eData, Spell } from '@/types';
+
+const props = defineProps<{
+  data: Open5eData;
+  category?: ComputedRef<string> | string;
+}>();
+
+const endpointToCategoryDisplayNameMap = {
+  '/monsters': 'Monster',
+  '/species': 'Species',
+  '/classes': 'Class',
+  '/spells': 'Spell',
+  '/magic-items': 'Magic Item',
+  '/equipment': 'Equipment',
+  '/': '',
+};
+
+const dataType = unref(props?.category ?? '/') as keyof typeof endpointToCategoryDisplayNameMap;
+
+const categoryDisplayName = computed(() => {
+  const category = endpointToCategoryDisplayNameMap[dataType];
+  if (category) return category;
+  if (dataType.includes('classes')) {
+    return `${(props.data as Class).subclass_of.name} Subclass`;
+  }
+  return '';
 });
 
-const subtitle = computed(() => {
-  const data = props.content;
-  switch (props.category) {
-    case 'spells':
-      return `${data.level} ${data.school} Spell`;
-    case 'monsters':
-      return 'Monster';
-    case 'magicitems':
-      return 'Magic Item';
-    case 'conditions':
-      return 'Condition';
-    case 'feats':
-      return 'Feat';
-    case 'sections':
-    case 'characters':
-    case 'combat':
-    case 'equipment':
-    case 'gameplay-mechanics':
-    case 'running':
-      return data.parent;
-    default:
-      return props.category.charAt(0).toUpperCase() + props.category.slice(1);
+const subtitle = computed<string>(() => {
+  if (dataType === '/monsters') {
+    const monster = props.data as Creature;
+    return `${monster.size.name} ${monster.type.name} (CR ${monster.challenge_rating_text})`;
   }
+  if (dataType === '/spells') {
+    const spell = props.data as Spell;
+    if (spell.level === 0) return `${spell.school.name} Cantrip`;
+    return `Level ${spell.level} ${spell.school.name} Spell`;
+  }
+  if (dataType === '/magic-items') {
+    const item = props.data as Item;
+    return `${item.category.name}, ${item.rarity.name}`;
+  }
+  
+  return '';
 });
 
-const body = computed(() => {
-  const data = props.content;
-  switch (props.category) {
-    case 'conditions':
-      return [{ data: data.desc }];
-    case 'spells':
-      return [
-        { title: 'Casting Time', data: data.casting_time },
-        { title: 'Duration', data: data.duration },
-        { title: 'Range', data: data.range },
-        { title: 'Components', data: data.components },
-      ];
-    case 'magicitems':
-      return [
-        {
-          data: `${data.type}, ${data.rarity} ${
-            data.requires_attunement && '(requires attunement)'
-          }`,
-        },
-      ];
-    case 'monsters':
-      return [
-        { data: `CR ${data.challenge_rating} ${data.type} (${data.size})` },
-      ];
-    default:
-      return [];
-  }
-});
 </script>
 
-<style></style>
